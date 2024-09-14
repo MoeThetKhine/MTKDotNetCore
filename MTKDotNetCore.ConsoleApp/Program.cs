8cd66c0ce1bb5806aa0486cebf81884c1b5417ab
Moe Thet Khine <soniapoudel03@gmail.com>
2024-09-14 09:07:21 +0630

    Feat: Added ConnectionString

--- MTKDotNetCore.ConsoleApp/Program.cs
+++ MTKDotNetCore.ConsoleApp/Program.cs
@@ -1,3 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
-Console.ReadLine();
-Console.ReadKey();
+﻿using System.Data.SqlClient;
+
+string connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;";
+//Console.WriteLine ($"Connection String : + { connectionString}");
+Console.WriteLine("Connection String :" + connectionString);
+
+SqlConnection connection = new SqlConnection(connectionString);
+
+Console.WriteLine("Connection Opening");
+connection.Open();
+Console.WriteLine("Connection Opened");
+
+
+Console.WriteLine("Connection Closing");
+connection.Close();
+Console.WriteLine("Connection Closed");
