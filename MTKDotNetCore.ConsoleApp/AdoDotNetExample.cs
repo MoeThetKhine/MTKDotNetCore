e2ec511933b46b7695ef0b0d47c474f85bd35236
Moe Thet Khine <soniapoudel03@gmail.com>
2024-09-15 11:39:02 +0630

    Feat : Add Ado.Net Read

---
+++ MTKDotNetCore.ConsoleApp/AdoDotNetExample.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKDotNetCore.ConsoleApp
+{
+    public class AdoDotNetExample
+    {
+        private readonly string _connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;";
+
+        public void Read()
+        {
+            Console.WriteLine("Connection String :" + _connectionString);
+
+            SqlConnection connection = new SqlConnection(_connectionString);
+            Console.WriteLine("Connection Opening");
+            connection.Open();
+            Console.WriteLine("Connection Opened");
+
+            string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog] where DeleteFlag = 0";
+
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine(reader["BlogId"]);
+                Console.WriteLine(reader["BlogTitle"]);
+                Console.WriteLine(reader["BlogAuthor"]);
+                Console.WriteLine(reader["BlogContent"]);
+            }
+            Console.WriteLine("Connection Closing");
+            connection.Close();
+            Console.WriteLine("Connection Closed");
+        }
+
+       
+    }
+}
--- MTKDotNetCore.ConsoleApp/Program.cs
+++ MTKDotNetCore.ConsoleApp/Program.cs
@@ -1,59 +1,62 @@
-﻿using System.Data;
+﻿using MTKDotNetCore.ConsoleApp;
+using System.Data;
 using System.Data.SqlClient;
+
+#region Day2
 
 #region ConnectionString
 
-string connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;";
+//string connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;";
 //Console.WriteLine ($"Connection String : + { connectionString}");
-Console.WriteLine("Connection String :" + connectionString);
+//Console.WriteLine("Connection String :" + connectionString);
 
 #endregion
 
 
-SqlConnection connection = new SqlConnection(connectionString);
+//SqlConnection connection = new SqlConnection(connectionString);
 
 #region ConnectionOpen
 
-Console.WriteLine("Connection Opening");
-connection.Open();
-Console.WriteLine("Connection Opened");
+//Console.WriteLine("Connection Opening");
+//connection.Open();
+//Console.WriteLine("Connection Opened");
 
 #endregion
 
 #region query
 
-string query = @"SELECT [BlogId]
-      ,[BlogTitle]
-      ,[BlogAuthor]
-      ,[BlogContent]
-      ,[DeleteFlag]
-  FROM [dbo].[Tbl_Blog] where DeleteFlag = 0;";
+//string query = @"SELECT [BlogId]
+//      ,[BlogTitle]
+//      ,[BlogAuthor]
+//      ,[BlogContent]
+//      ,[DeleteFlag]
+//  FROM [dbo].[Tbl_Blog] where DeleteFlag = 0;";
 
 #endregion
 
 #region Executing
 
-SqlCommand cmd = new SqlCommand(query, connection);
-//SqlDataAdapter adapter = new SqlDataAdapter(cmd);
-//DataTable dt = new DataTable();
-//adapter.Fill(dt); (or) dt = adapter.Execute();
-SqlDataReader reader = cmd.ExecuteReader();
+//SqlCommand cmd = new SqlCommand(query, connection);
+////SqlDataAdapter adapter = new SqlDataAdapter(cmd);
+////DataTable dt = new DataTable();
+////adapter.Fill(dt); (or) dt = adapter.Execute();
+//SqlDataReader reader = cmd.ExecuteReader();
 
-while (reader.Read())
-{
-    Console.WriteLine(reader["BlogId"]);
-    Console.WriteLine(reader["BlogTitle"]);
-    Console.WriteLine(reader["BlogAuthor"]);
-    Console.WriteLine(reader["BlogContent"]);
-}
+//while (reader.Read())
+//{
+//    Console.WriteLine(reader["BlogId"]);
+//    Console.WriteLine(reader["BlogTitle"]);
+//    Console.WriteLine(reader["BlogAuthor"]);
+//    Console.WriteLine(reader["BlogContent"]);
+//}
 
 #endregion
 
 #region ConnectionClose
 
-Console.WriteLine("Connection Closing");
-connection.Close();
-Console.WriteLine("Connection Closed");
+//Console.WriteLine("Connection Closing");
+//connection.Close();
+//Console.WriteLine("Connection Closed");
 
 #endregion
 
@@ -66,4 +69,13 @@ Console.WriteLine("Connection Closed");
 
 //}
 
-Console.ReadKey();
+//Console.ReadKey();
+
+#endregion
+
+AdoDotNetExample adoDotNetExample = new AdoDotNetExample();
+//adoDotNetExample.Read();
+//adoDotNetExample.Create();
+//adoDotNetExample.Update();
+//adoDotNetExample.Edit();
+adoDotNetExample.Delete();
