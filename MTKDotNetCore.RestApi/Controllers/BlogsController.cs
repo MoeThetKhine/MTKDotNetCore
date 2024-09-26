6d65d2a7e2eef9c7e8f46eb320aed624bb31922b
Moe Thet Khine <soniapoudel03@gmail.com>
2024-09-26 21:18:29 +0630

    Feat : Get Blogs

---
+++ MTKDotNetCore.RestApi/Controllers/BlogsController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MTKDotNetCore.Database.Models;
+
+namespace MTKDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogsController : ControllerBase
+    {
+        private readonly AppDbContext _db = new AppDbContext();
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            var lst = _db.TblBlogs
+                .AsNoTracking ()
+                .Where(x => !x.DeleteFlag)
+                .ToList ();
+            return Ok (lst);
+        }
+    }
+}
