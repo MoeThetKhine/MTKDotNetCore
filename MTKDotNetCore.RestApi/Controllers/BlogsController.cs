53b96f5e921a375b015845ad7fab9992368f51fe
Moe Thet Khine <soniapoudel03@gmail.com>
2024-09-28 21:20:32 +0630

    Complete Api CRUD

--- MTKDotNetCore.RestApi/Controllers/BlogsController.cs
+++ MTKDotNetCore.RestApi/Controllers/BlogsController.cs
@@ -2,123 +2,122 @@
 using Microsoft.EntityFrameworkCore;
 using MTKDotNetCore.Database.Models;
 
-namespace MTKDotNetCore.RestApi.Controllers
+namespace MTKDotNetCore.RestApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BlogsController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BlogsController : ControllerBase
+    private readonly AppDbContext _db = new AppDbContext();
+
+    #region GetBlogs
+
+    [HttpGet]
+    public IActionResult GetBlogs()
     {
-        private readonly AppDbContext _db = new AppDbContext();
-
-        #region GetBlogs
-
-        [HttpGet]
-        public IActionResult GetBlogs()
-        {
-            var lst = _db.TblBlogs
-                .AsNoTracking ()
-                .Where(x => !x.DeleteFlag)
-                .ToList ();
-            return Ok (lst);
-        }
-
-        #endregion
-
-        #region GetBlog(Edit)
-
-        [HttpGet("{id}")]
-        public IActionResult GetBlog(int id)
-        {
-
-            var item = _db.TblBlogs
-                .AsNoTracking()
-                .FirstOrDefault(x => x.BlogId == id);
-            if(item is null)
-            {
-                return NotFound();
-            }
-            return Ok (item);
-        }
-
-        #endregion
-
-        #region CreateBlog
-
-        [HttpPost]
-        public IActionResult CreateBlog(TblBlog blog)
-        {
-            _db.TblBlogs.Add(blog);
-            _db.SaveChanges();
-            return Ok(blog);
-        }
-
-        #endregion
-
-        #region Update Blog
-
-        [HttpPut("{id}")]
-        public IActionResult UpdateBlog(int id, TblBlog blog)
-        {
-            var item = _db.TblBlogs.AsNoTracking()
-                .FirstOrDefault(x => x.BlogId == id);
-            if(item is null)
-            {
-                return NotFound();
-            }
-            item.BlogTitle = blog.BlogTitle;
-            item.BlogAuthor = blog.BlogAuthor;
-            item.BlogContent = blog.BlogContent;
-
-            _db.Entry(item).State = EntityState.Modified;
-            _db.SaveChanges();
-            return Ok(item);
-        }
-
-        #endregion
-
-        #region Patch Blog
-
-        [HttpPatch("{id}")]
-        public IActionResult PatchBlog(int id,TblBlog blog)
-        {
-            var item = _db.TblBlogs.AsNoTracking()
-                .FirstOrDefault (x => x.BlogId == id);
-            if(item is null)
-            {
-                return NotFound();
-            }
-            if(!string.IsNullOrEmpty(blog.BlogTitle))
-            {
-                item.BlogTitle = blog.BlogTitle;
-            }
-            if (!string.IsNullOrEmpty(blog.BlogAuthor))
-            {
-                item.BlogAuthor = blog.BlogAuthor;
-            }
-            _db.Entry (item).State = EntityState.Modified;
-            _db.SaveChanges();
-            return Ok(item);
-        }
-
-        #endregion
-
-        #region Delete Blog
-
-        [HttpDelete("{id}")]
-        public IActionResult DeleteBlog(int id)
-        {
-            var item = _db.TblBlogs.AsNoTracking()
-                .FirstOrDefault(x=> x.BlogId == id);
-            if(item is null)
-            {
-                return NotFound();
-            }
-            item.DeleteFlag = true;
-            _db.Entry(item) .State = EntityState.Modified;
-            _db.SaveChanges();
-            return Ok();
-        }
-
-        #endregion
+        var lst = _db.TblBlogs
+            .AsNoTracking ()
+            .Where(x => !x.DeleteFlag)
+            .ToList ();
+        return Ok (lst);
     }
+
+    #endregion
+
+    #region GetBlog(Edit)
+
+    [HttpGet("{id}")]
+    public IActionResult GetBlog(int id)
+    {
+
+        var item = _db.TblBlogs
+            .AsNoTracking()
+            .FirstOrDefault(x => x.BlogId == id);
+        if(item is null)
+        {
+            return NotFound();
+        }
+        return Ok (item);
+    }
+
+    #endregion
+
+    #region CreateBlog
+
+    [HttpPost]
+    public IActionResult CreateBlog(TblBlog blog)
+    {
+        _db.TblBlogs.Add(blog);
+        _db.SaveChanges();
+        return Ok(blog);
+    }
+
+    #endregion
+
+    #region Update Blog
+
+    [HttpPut("{id}")]
+    public IActionResult UpdateBlog(int id, TblBlog blog)
+    {
+        var item = _db.TblBlogs.AsNoTracking()
+            .FirstOrDefault(x => x.BlogId == id);
+        if(item is null)
+        {
+            return NotFound();
+        }
+        item.BlogTitle = blog.BlogTitle;
+        item.BlogAuthor = blog.BlogAuthor;
+        item.BlogContent = blog.BlogContent;
+
+        _db.Entry(item).State = EntityState.Modified;
+        _db.SaveChanges();
+        return Ok(item);
+    }
+
+    #endregion
+
+    #region Patch Blog
+
+    [HttpPatch("{id}")]
+    public IActionResult PatchBlog(int id,TblBlog blog)
+    {
+        var item = _db.TblBlogs.AsNoTracking()
+            .FirstOrDefault (x => x.BlogId == id);
+        if(item is null)
+        {
+            return NotFound();
+        }
+        if(!string.IsNullOrEmpty(blog.BlogTitle))
+        {
+            item.BlogTitle = blog.BlogTitle;
+        }
+        if (!string.IsNullOrEmpty(blog.BlogAuthor))
+        {
+            item.BlogAuthor = blog.BlogAuthor;
+        }
+        _db.Entry (item).State = EntityState.Modified;
+        _db.SaveChanges();
+        return Ok(item);
+    }
+
+    #endregion
+
+    #region Delete Blog
+
+    [HttpDelete("{id}")]
+    public IActionResult DeleteBlog(int id)
+    {
+        var item = _db.TblBlogs.AsNoTracking()
+            .FirstOrDefault(x=> x.BlogId == id);
+        if(item is null)
+        {
+            return NotFound();
+        }
+        item.DeleteFlag = true;
+        _db.Entry(item) .State = EntityState.Modified;
+        _db.SaveChanges();
+        return Ok();
+    }
+
+    #endregion
 }
