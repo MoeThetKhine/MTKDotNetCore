afaa6eb0417214e7bb7c120b47bd9a23e2a7eefd
Moe Thet Khine <soniapoudel03@gmail.com>
2024-10-12 19:05:30 +0630

    Feat: Add AdoDotNet (Read)

---
+++ MTKDotNetCore.RestApi/Controllers/BlogAdoDotNetController.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using MTKDotNetCore.RestApi.ViewModels;
+
+namespace MTKDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogAdoDotNetController : ControllerBase
+    {
+        private readonly string _connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
+
+        [HttpGet]
+        public IActionResult GetBlog()
+        {
+            List<BlogViewModel>lst = new List<BlogViewModel>();
+            SqlConnection connection = new SqlConnection(_connectionString);
+            connection.Open();
+            string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog] where DeleteFlag = 0";
+            SqlCommand cmd = new SqlCommand(query, connection);
+            SqlDataReader reader = cmd.ExecuteReader();
+            while(reader.Read())
+            {
+                Console.WriteLine(reader["BlogId"]);
+                Console.WriteLine(reader["BlogTitle"]);
+                Console.WriteLine(reader["BlogAuthor"]);
+                Console.WriteLine(reader["BlogContent"]);
+
+                var item = new BlogViewModel
+                {
+                    BlogId = Convert.ToInt32(reader["BlogId"]),
+                    BlogTitle = Convert.ToString(reader["BlogTitle"]),
+                    BlogAuthor = Convert.ToString(reader["BlogAuthor"]),
+                    BlogContent = Convert.ToString(reader["BlogContent"]),
+                    DeleteFlag = Convert.ToBoolean(reader["DeleteFlag"]),
+                };
+                lst.Add(item);
+            }
+            connection.Close();
+            return Ok(lst);
+        }
+    }
+}
---
+++ MTKDotNetCore.RestApi/ViewModels/BlogViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace MTKDotNetCore.RestApi.ViewModels
+{
+    public class BlogViewModel
+    {
+        public int BlogId { get; set; }
+        public string? BlogTitle { get; set; }
+        public string? BlogAuthor { get; set; }
+        public string? BlogContent { get; set; }
+        public bool DeleteFlag { get; set; }
+    }
+}
