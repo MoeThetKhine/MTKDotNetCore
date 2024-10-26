18fc9fa4119573141249c1039f0b2420014b2c16
Moe Thet Khine <soniapoudel03@gmail.com>
2024-10-26 19:09:28 +0630

    Feat : Add ToDoListDataModel

---
+++ MTKDotNetCore.ToDoListRestApi/DataModels/ToDoListDataModel.cs
@@ -0,0 +1,15 @@
+﻿namespace MTKDotNetCore.ToDoListRestApi.DataModels
+{
+    public class ToDoListDataModel
+    {
+        public int TaskID {  get; set; }
+        public string TaskTitle { get; set; }
+        public string TaskDescription { get; set; }
+        public int? CategoryId { get; set; }
+        public byte? PriorityLevel { get; set; }
+        public string Status { get; set; }
+        public DateTime? DueDate { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime? CompletedDate { get; set; }
+    }
+}
