b8d48512d9b0572667fde7d017ad13ec4487f2a5
Moe Thet Khine <soniapoudel03@gmail.com>
2024-10-26 19:07:28 +0630

    Feat: Add CategoryViewModel

---
+++ MTKDotNetCore.ToDoListRestApi/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MTKDotNetCore.ToDoListRestApi.ViewModels
+{
+    public class CategoryViewModel
+    {
+       // public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+       // public bool IsDelete { get; set; }
+    }
+}
