6f4e8111745381ee0d94f672468d79b138515bbb
Moe Thet Khine <soniapoudel03@gmail.com>
2024-10-26 19:09:59 +0630

    Feat : Add CategoryDataModel

---
+++ MTKDotNetCore.ToDoListRestApi/DataModels/CategoryDataModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MTKDotNetCore.ToDoListRestApi.DataModels
+{
+    public class CategoryDataModel
+    {
+        public int CategoryId { get; set; }
+        public string CategoryName { get; set; }
+        public bool IsDelete { get; set; }
+    }
+}
