45d65b32ea3aab143c9eeabeffe6c7694f48044b
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-01 11:59:33 +0630

    Feat: Add SqlParameterModel

---
+++ MTKDotNetCore.Shared/AdoDotNetService.cs
@@ -0,0 +1,33 @@
+﻿using System.Data;
+using System.Data.SqlClient;
+
+namespace MTKDotNetCore.Shared
+{
+    public class AdoDotNetService
+    {
+        private readonly string _connectionString;
+
+        public AdoDotNetService(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+       
+    }
+
+    #region SqlParameterModel
+
+    public class SqlParameterModel
+    {
+        public string Name { get; set; }
+        public object Value { get; set; }
+
+        public SqlParameterModel() { }
+        public SqlParameterModel(string name, object value)
+        {
+            Name = name;
+            Value = value;
+        }
+    }
+    #endregion
+}
