1b9074cf28a7e721e77cadd6596eb6f427a241ba
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-04 23:04:55 +0630

    Code Format

--- MTKDotNetCore.ConsoleApp/AdoDotNetExampe2.cs
+++ MTKDotNetCore.ConsoleApp/AdoDotNetExampe2.cs
@@ -1,81 +1,81 @@
 ﻿using MTKDotNetCore.Shared;
 using System.Data;
 
-namespace MTKDotNetCore.ConsoleApp
+namespace MTKDotNetCore.ConsoleApp;
+
+public class AdoDotNetExampe2
 {
-    public class AdoDotNetExampe2
+    private readonly string _connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;";
+
+    private readonly AdoDotNetService _adoDotNetService;
+
+    public AdoDotNetExampe2()
     {
-        private readonly string _connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;";
+        _adoDotNetService = new AdoDotNetService(_connectionString);
+    }
 
-        private readonly AdoDotNetService _adoDotNetService;
+    #region Read
 
-        public AdoDotNetExampe2()
-        {
-            _adoDotNetService = new AdoDotNetService(_connectionString);
-        }
-
-        #region Read
-
-        public void Read()
-        {
-            string query = @"SELECT [BlogId]
+    public void Read()
+    {
+        string query = @"SELECT [BlogId]
       ,[BlogTitle]
       ,[BlogAuthor]
       ,[BlogContent]
       ,[DeleteFlag]
   FROM [dbo].[Tbl_Blog] where DeleteFlag = 0";
-           var dt = _adoDotNetService.Query(query);
-            foreach(DataRow dr in dt.Rows)
-            {
-                Console.WriteLine(dr["BlogId"]);
-                Console.WriteLine(dr["BlogTitle"]);
-                Console.WriteLine(dr["BlogAuthor"]);
-                Console.WriteLine(dr["BlogContent"]);
-            }
-        }
-
-        #endregion
-
-        #region Edit
-
-        public void Edit() 
+       var dt = _adoDotNetService.Query(query);
+        foreach(DataRow dr in dt.Rows)
         {
-            Console.Write("Blog Id: ");
-            string id = Console.ReadLine();
-
-            string query = @"SELECT [BlogId]
-      ,[BlogTitle]
-      ,[BlogAuthor]
-      ,[BlogContent]
-      ,[DeleteFlag]
-  FROM [dbo].[Tbl_Blog] where BlogId = @BlogId";
-            var dt = _adoDotNetService.Query(query,
-                new SqlParameterModel("@BlogId", id));
-            
-            DataRow dr = dt.Rows[0];
             Console.WriteLine(dr["BlogId"]);
             Console.WriteLine(dr["BlogTitle"]);
             Console.WriteLine(dr["BlogAuthor"]);
             Console.WriteLine(dr["BlogContent"]);
         }
+    }
 
-        #endregion
+    #endregion
 
-        #region Create
+    #region Edit
 
-        public void Create()
-        {
-            Console.WriteLine("Blog Title: ");
-            string title = Console.ReadLine();
+    public void Edit() 
+    {
+        Console.Write("Blog Id: ");
+        string id = Console.ReadLine();
 
-            Console.WriteLine("Blog Author: ");
-            string author = Console.ReadLine();
+        string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog] where BlogId = @BlogId";
+        var dt = _adoDotNetService.Query(query,
+            new SqlParameterModel("@BlogId", id));
+        
+        DataRow dr = dt.Rows[0];
+        Console.WriteLine(dr["BlogId"]);
+        Console.WriteLine(dr["BlogTitle"]);
+        Console.WriteLine(dr["BlogAuthor"]);
+        Console.WriteLine(dr["BlogContent"]);
+    }
 
-            Console.WriteLine("Blog Content: ");
-            string content = Console.ReadLine();
+    #endregion
+
+    #region Create
+
+    public void Create()
+    {
+        Console.WriteLine("Blog Title: ");
+        string title = Console.ReadLine();
+
+        Console.WriteLine("Blog Author: ");
+        string author = Console.ReadLine();
+
+        Console.WriteLine("Blog Content: ");
+        string content = Console.ReadLine();
 
 
-            string query = $@"INSERT INTO [dbo].[Tbl_Blog]
+        string query = $@"INSERT INTO [dbo].[Tbl_Blog]
           ([BlogTitle]
           ,[BlogAuthor]
           ,[BlogContent]
@@ -85,71 +85,68 @@ namespace MTKDotNetCore.ConsoleApp
           ,@BlogAuthor
           ,@BlogContent
           ,0)";
-           
-            int result = _adoDotNetService.Execute(query,
+       
+        int result = _adoDotNetService.Execute(query,
 
-            new SqlParameterModel("@BlogTitle", title),
-            new SqlParameterModel("@BlogAuthor", author),
-            new SqlParameterModel("@BlogContent", content));
+        new SqlParameterModel("@BlogTitle", title),
+        new SqlParameterModel("@BlogAuthor", author),
+        new SqlParameterModel("@BlogContent", content));
 
-            Console.WriteLine(result == 1 ? "Creating Successful." : "Creating Failed.");
-        }
+        Console.WriteLine(result == 1 ? "Creating Successful." : "Creating Failed.");
+    }
 
-        #endregion
+    #endregion
 
-        #region Delete
+    #region Delete
 
-        public void Delete()
-        {
-            Console.WriteLine("Blog Id:");
-            string id = Console.ReadLine();
+    public void Delete()
+    {
+        Console.WriteLine("Blog Id:");
+        string id = Console.ReadLine();
 
-            string query = @"UPDATE [dbo].[Tbl_Blog]
+        string query = @"UPDATE [dbo].[Tbl_Blog]
                      SET [DeleteFlag] = 1
                      WHERE BlogId = @BlogId";
 
-            int result = _adoDotNetService.Execute(query, new SqlParameterModel("@BlogId", id));
+        int result = _adoDotNetService.Execute(query, new SqlParameterModel("@BlogId", id));
 
-            Console.WriteLine(result == 1 ? "Deleting Successful." : "Deleting Failed.");
-        }
+        Console.WriteLine(result == 1 ? "Deleting Successful." : "Deleting Failed.");
+    }
 
-        #endregion
+    #endregion
 
-        #region Update
+    #region Update
 
-        public void Update()
-        {
-            Console.WriteLine("Blog Id:");
-            string id = Console.ReadLine();
+    public void Update()
+    {
+        Console.WriteLine("Blog Id:");
+        string id = Console.ReadLine();
 
-            Console.WriteLine("Blog Title:");
-            string title = Console.ReadLine();
+        Console.WriteLine("Blog Title:");
+        string title = Console.ReadLine();
 
-            Console.WriteLine("Blog Author:");
-            string author = Console.ReadLine();
+        Console.WriteLine("Blog Author:");
+        string author = Console.ReadLine();
 
-            Console.WriteLine("Blog Content:");
-            string content = Console.ReadLine();
+        Console.WriteLine("Blog Content:");
+        string content = Console.ReadLine();
 
-            string query = @"UPDATE [dbo].[Tbl_Blog]
+        string query = @"UPDATE [dbo].[Tbl_Blog]
                      SET [BlogTitle] = @BlogTitle,
                          [BlogAuthor] = @BlogAuthor,
                          [BlogContent] = @BlogContent,
                          [DeleteFlag] = 0
                      WHERE BlogId = @BlogId";
 
-            int result = _adoDotNetService.Execute(query,
-                new SqlParameterModel("@BlogId", id),
-                new SqlParameterModel("@BlogTitle", title),
-                new SqlParameterModel("@BlogAuthor", author),
-                new SqlParameterModel("@BlogContent", content));
+        int result = _adoDotNetService.Execute(query,
+            new SqlParameterModel("@BlogId", id),
+            new SqlParameterModel("@BlogTitle", title),
+            new SqlParameterModel("@BlogAuthor", author),
+            new SqlParameterModel("@BlogContent", content));
 
-            Console.WriteLine(result == 1 ? "Updating Successful." : "Updating Failed.");
-        }
-
-        #endregion
-
-
-
+        Console.WriteLine(result == 1 ? "Updating Successful." : "Updating Failed.");
     }
+
+    #endregion
+
 }
