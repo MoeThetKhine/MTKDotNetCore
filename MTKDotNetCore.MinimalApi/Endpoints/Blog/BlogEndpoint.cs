d0af3df77e2422a9f4ce24aefc605cb3c0f15ba7
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-05 16:24:38 +0630

    Feat : Add GetBlog MinimalApi

---
+++ MTKDotNetCore.MinimalApi/Endpoints/Blog/BlogEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTKDotNetCore.Database.Models;
+
+namespace MTKDotNetCore.MinimalApi.Endpoints.Blog
+{
+    public static class BlogEndpoint
+    {
+        public static void MapBlogEndpoint(this IEndpointRouteBuilder app)
+        {
+
+            #region GetBlog 
+
+            app.MapGet("/blogs", () =>
+            {
+                AppDbContext db = new AppDbContext();
+                var model = db.TblBlogs.AsNoTracking().ToList();
+                return Results.Ok(model);
+
+            }).WithName("GetBlog")
+            .WithOpenApi();
+
+            #endregion
+
+
+        }
+    }
+}
