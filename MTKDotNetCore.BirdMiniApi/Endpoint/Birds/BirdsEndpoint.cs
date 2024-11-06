5b481facf7eef4f25a2dbca6c7796e9eaa6cf54c
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-07 01:41:46 +0630

    Feat : Add Get Birds in Birds Endpoint

--- MTKDotNetCore.BirdMiniApi/Endpoint/Birds/BirdsEndpoint.cs
+++ MTKDotNetCore.BirdMiniApi/Endpoint/Birds/BirdsEndpoint.cs
@@ -4,7 +4,21 @@
     {
         public static void MapBirdsEndpoint(this IEndpointRouteBuilder app)
         {
+            #region GetBirds
 
+            app.MapGet("/birds", () =>
+            {
+                string folderPath = "Data/Bird.json";
+
+                var jsonStr = File.ReadAllText(folderPath);
+                var result = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr)!;
+
+                return Results.Ok(result.Tbl_Bird);
+
+            }).WithName("GetBirds")
+            .WithOpenApi();
+
+            #endregion
         }
     }
 }
--- MTKDotNetCore.BirdMiniApi/Program.cs
+++ MTKDotNetCore.BirdMiniApi/Program.cs
@@ -13,21 +13,7 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-#region GetBirds
 
-app.MapGet("/birds", () =>
-{
-    string folderPath = "Data/Bird.json";
-
-    var jsonStr = File.ReadAllText(folderPath);
-    var result = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr)!;
-
-    return Results.Ok(result.Tbl_Bird);
-
-}).WithName("GetBirds")
-.WithOpenApi();
-
-#endregion
 
 #region Edit Birds
 
