56f2c6a776b39898fe499d0f6cbfd959a57de555
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-07 01:38:39 +0630

    Feat : Add Edit Birds

--- MTKDotNetCore.BirdMiniApi/Program.cs
+++ MTKDotNetCore.BirdMiniApi/Program.cs
@@ -17,13 +17,35 @@ app.UseHttpsRedirection();
 
 app.MapGet("/birds", () =>
 {
-
     string folderPath = "Data/Bird.json";
+
     var jsonStr = File.ReadAllText(folderPath);
     var result = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr)!;
+
     return Results.Ok(result.Tbl_Bird);
 
 }).WithName("GetBirds")
+.WithOpenApi();
+
+#endregion
+
+#region Edit Birds
+
+app.MapGet("/birds/{id}", (int id) =>
+{
+    string folderPath = "Data/Bird.json";
+
+    var jsonStr = File.ReadAllText(folderPath);
+    var result = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr)!;
+
+    var item = result.Tbl_Bird.FirstOrDefault(x=> x.Id == id);
+
+    if(item is null)
+    {
+        return Results.BadRequest("No Data Found");
+    }
+    return Results.Ok(item);
+}).WithName("EditBird")
 .WithOpenApi();
 
 #endregion
