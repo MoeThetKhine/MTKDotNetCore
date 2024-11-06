41d8ddb17e5f8d0f1f32d9526b8aad03923881e0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-07 01:29:04 +0630

    Feat: Add Get Birds

--- MTKDotNetCore.BirdMiniApi/Program.cs
+++ MTKDotNetCore.BirdMiniApi/Program.cs
@@ -1,13 +1,13 @@
+using MTKDotNetCore.BirdMiniApi.Models;
+using Newtonsoft.Json;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -16,29 +16,19 @@ if (app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+#region GetBirds
 
-app.MapGet("/weatherforecast", () =>
+app.MapGet("/birds", () =>
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-        new WeatherForecast
-        (
-            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            Random.Shared.Next(-20, 55),
-            summaries[Random.Shared.Next(summaries.Length)]
-        ))
-        .ToArray();
-    return forecast;
-})
-.WithName("GetWeatherForecast")
+
+    string folderPath = "Data/Bird.json";
+    var jsonStr = File.ReadAllText(folderPath);
+    var result = JsonConvert.DeserializeObject<BirdResponseModel>(jsonStr)!;
+    return Results.Ok(result.Tbl_Bird);
+
+}).WithName("GetBirds")
 .WithOpenApi();
 
-app.Run();
+#endregion
 
-internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
+app.Run();
