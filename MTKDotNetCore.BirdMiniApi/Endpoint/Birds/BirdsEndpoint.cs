0bf7d09cf07318359cccf4c88cac141d51db4715
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-07 01:41:01 +0630

    Feat : Add Birds Endpoint

---
+++ MTKDotNetCore.BirdMiniApi/Endpoint/Birds/BirdsEndpoint.cs
@@ -0,0 +1,10 @@
+﻿namespace MTKDotNetCore.BirdMiniApi.Endpoint.Birds
+{
+    public static class BirdsEndpoint
+    {
+        public static void MapBirdsEndpoint(this IEndpointRouteBuilder app)
+        {
+
+        }
+    }
+}
