f34480f0398f64790c083a3fbc4b9ccb77887c58
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-07 02:07:07 +0630

    Feat : DreamAns Model

---
+++ MTKDotNetCore.DreamDictionaryMinimalApi/Models/DreamAns.cs
@@ -0,0 +1,9 @@
+﻿namespace MTKDotNetCore.DreamDictionaryMinimalApi.Models
+{
+    public class DreamAns
+    {
+        public int BlogDetailId { get; set; }
+        public int BlogId { get; set; }
+        public string BlogContent { get; set; }
+    }
+}
