3b7c3095b105c819818057d422793de3c7bd9d96
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-07 01:31:21 +0630

    Feat : Update Bird Model

--- MTKDotNetCore.BirdMiniApi/Models/BirdModel.cs
+++ MTKDotNetCore.BirdMiniApi/Models/BirdModel.cs
@@ -2,5 +2,10 @@
 {
     public class BirdModel
     {
+        public int Id { get; set; }
+        public string BirdMyanmarName { get; set; }
+        public string BirdEnglishName { get; set; }
+        public string Description { get; set; }
+        public string ImagePath { get; set; }
     }
 }
