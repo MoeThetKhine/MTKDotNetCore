c9182642704703f4686c35fd2055f4c319643795
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-07 16:04:39 +0630

    Feat : Add Get Dream Title

---
+++ MTKDotNetCore.DreamDictionaryMinimalApi/Endpoint/Dreams/DreamsEndpoint.cs
@@ -0,0 +1,27 @@
+﻿using MTKDotNetCore.DreamDictionaryMinimalApi.Models;
+using Newtonsoft.Json;
+
+namespace MTKDotNetCore.DreamDictionaryMinimalApi.Endpoint.Dreams
+{
+    public static class DreamsEndpoint
+    {
+        public static void MapDreamsEndpoint(this IEndpointRouteBuilder app)
+        {
+            #region Get Dream Title
+
+            app.MapGet("/dreams", () =>
+            {
+                string folderPath = "Data/dreams.json";
+
+                var jsonStr = File.ReadAllText(folderPath);
+                var result = JsonConvert.DeserializeObject<DreamsResponseModel>(jsonStr)!;
+
+                return Results.Ok(result.BlogHeader);
+
+            }).WithName("GetBirds")
+            .WithOpenApi();
+
+            #endregion
+        }
+    }
+}
