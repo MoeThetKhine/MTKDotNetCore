a2dae21ad8672287fd3af7e426d61b45a9562729
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-15 02:03:52 +0630

    Feat : Add Global and Format Code

--- MTKDotNetCore.Domain/Features/Blog/BlogService.cs
+++ MTKDotNetCore.Domain/Features/Blog/BlogService.cs
@@ -1,21 +1,17 @@
-﻿using Microsoft.EntityFrameworkCore;
-using MTKDotNetCore.Database.Models;
+﻿namespace MTKDotNetCore.Domain.Features.Blog;
 
-namespace MTKDotNetCore.Domain.Features.Blog
+public class BlogService
 {
-    public class BlogService
+    private readonly AppDbContext _db = new AppDbContext();
+
+    #region GetBlog
+
+    public List<TblBlog> GetBlogs()
     {
-        private readonly AppDbContext _db = new AppDbContext();
-
-        #region GetBlog
-
-        public List<TblBlog> GetBlogs()
-        {
-            var model = _db.TblBlogs.AsNoTracking().ToList();
-            return model;
-        }
-
-        #endregion
-        
+        var model = _db.TblBlogs.AsNoTracking().ToList();
+        return model;
     }
+
+    #endregion
+    
 }
