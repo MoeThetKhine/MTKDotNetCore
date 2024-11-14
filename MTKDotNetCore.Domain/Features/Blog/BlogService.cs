dbd097108d824d2327e1e744c8281de3be281591
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-15 02:01:56 +0630

    Feat : Add GetBlogs

---
+++ MTKDotNetCore.Domain/Features/Blog/BlogService.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTKDotNetCore.Database.Models;
+
+namespace MTKDotNetCore.Domain.Features.Blog
+{
+    public class BlogService
+    {
+        private readonly AppDbContext _db = new AppDbContext();
+
+        #region GetBlog
+
+        public List<TblBlog> GetBlogs()
+        {
+            var model = _db.TblBlogs.AsNoTracking().ToList();
+            return model;
+        }
+
+        #endregion
+        
+    }
+}
