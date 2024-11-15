81aa5db7d24888c73f0ecfdd23c874c76840aeb2
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-15 09:19:35 +0630

    Feat : Add Get Blogs in BlogService Controller

---
+++ MTKDotNetCore.RestApi/Controllers/BlogServiceController.cs
@@ -0,0 +1,28 @@
+﻿using MTKDotNetCore.Domain.Features.Blog;
+
+namespace MTKDotNetCore.RestApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogServiceController : ControllerBase
+    {
+        private readonly BlogService _service;
+
+        public BlogServiceController()
+        {
+            _service = new BlogService();
+        }
+
+        #region GetBlogs
+
+        [HttpGet]
+        public IActionResult GetBlogs()
+        {
+            var lst = _service.GetBlogs();
+            return Ok(lst);
+        }
+
+        #endregion
+
+    }
+}
