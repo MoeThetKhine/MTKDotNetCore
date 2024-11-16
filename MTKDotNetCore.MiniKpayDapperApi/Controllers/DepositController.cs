d5ad4184e9b5fbeac6fbde92842298a6545d7779
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 13:55:24 +0630

    Feat : Add Create Deposit

--- MTKDotNetCore.MiniKpayDapperApi/Controllers/DepositController.cs
+++ MTKDotNetCore.MiniKpayDapperApi/Controllers/DepositController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using MTKDotNetCore.MiniKpayDapperApi.Models.Deposit;
+using MTKDotNetCore.MiniKpayDapperApi.Models.User;
+using MTKDotNetCore.MiniKpayDapperApi.Models.Withdraw;
 
 namespace MTKDotNetCore.MiniKpayDapperApi.Controllers
 {
@@ -42,7 +44,60 @@ namespace MTKDotNetCore.MiniKpayDapperApi.Controllers
             return Ok(deposits);
         }
 
-        #endregion 
+        #endregion
+
+        #region CreateDeposit
+
+        [HttpPost]
+
+        public IActionResult CreateDeposit(DepositResponseModel deposit)
+        {
+
+            #region Check User with this PhNo Exist or not
+
+            string userQuery = "SELECT Balance FROM Tbl_User WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
+            var currentUser = _dapperService.QueryFirstOrDefault<UserModel>(userQuery,new {PhoneNumber = deposit.PhoneNumber});
+
+            #endregion
+
+            if (currentUser is null)
+            {
+                return BadRequest("User with this phone number does not exists.");
+            }
+            if(deposit.Balance <= 0)
+            {
+                return BadRequest("Deposit amount must be greater than 0.");
+            }
+
+            var updatedBalance = currentUser.Balance + deposit.Balance;
+
+            #region Update balance in user table
+
+            string updateBalanceQuery = "UPDATE Tbl_User SET Balance = @UpdatedBalance WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
+            int updateResult = _dapperService.Execute(updateBalanceQuery, new
+            {
+                UpdatedBalance = updatedBalance,  // Corrected to match the SQL query
+                PhoneNumber = deposit.PhoneNumber
+            });
+
+            #endregion
+
+            if (updateResult == 0)
+            {
+                return NotFound("Update the user balance failed.");
+            }
+            string insertDepositQuery = @"INSERT INTO Tbl_Deposit (PhoneNumber, Balance, DeleteFlag) VALUES (@PhoneNumber, @Balance, 0);";
+
+            int insertResult = _dapperService.Execute(insertDepositQuery, deposit);
+
+            if(insertResult == 0)
+            {
+                return BadRequest("Deposit process failed");
+            }
+            return Ok("Deposit process completed successfully.");
+        }
+
+        #endregion
 
     }
 }
--- MTKDotNetCore.MiniKpayDapperApi/Controllers/WithdrawController.cs
+++ MTKDotNetCore.MiniKpayDapperApi/Controllers/WithdrawController.cs
@@ -100,5 +100,6 @@ namespace MTKDotNetCore.MiniKpayDapperApi.Controllers
         }
 
         #endregion 
+
     }
 }
