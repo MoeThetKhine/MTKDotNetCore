1d888e7ac643deba3df09a4a782020147a99d01e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 15:14:35 +0630

    Format Code

--- MTKDotNetCore.MiniKpayDapperApi/Models/Deposit/DepositModel.cs
+++ MTKDotNetCore.MiniKpayDapperApi/Models/Deposit/DepositModel.cs
@@ -1,10 +1,9 @@
-﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.Deposit
+﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.Deposit;
+
+public class DepositModel
 {
-    public class DepositModel
-    {
-        public int Deposit_Id { get; set; }
-        public string PhoneNumber { get; set; }
-        public decimal Balance { get; set; }
-        public bool DeleteFlag { get; set; }
-    }
+    public int Deposit_Id { get; set; }
+    public string PhoneNumber { get; set; }
+    public decimal Balance { get; set; }
+    public bool DeleteFlag { get; set; }
 }
