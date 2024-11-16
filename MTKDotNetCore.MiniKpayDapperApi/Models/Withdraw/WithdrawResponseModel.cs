703a5f0129053121a634f764b83ea6669a27c280
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 09:56:57 +0630

    Feat: Add WithdrawResponseModel

---
+++ MTKDotNetCore.MiniKpayDapperApi/Models/Withdraw/WithdrawResponseModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.Withdraw
+{
+    public class WithdrawResponseModel
+    {
+        public string PhoneNumber { get; set; }
+        public decimal Balance { get; set; }
+        public bool DeleteFlag { get; set; }
+    }
+}
