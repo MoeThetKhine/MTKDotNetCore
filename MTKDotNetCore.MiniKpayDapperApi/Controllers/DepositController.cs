5fcd900568a0a480a3cdc9d4350d550aa116a97e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 11:53:37 +0630

    Feat : Add Get Deposits List

---
+++ MTKDotNetCore.MiniKpayDapperApi/Controllers/DepositController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MTKDotNetCore.MiniKpayDapperApi.Models.Deposit;
+
+namespace MTKDotNetCore.MiniKpayDapperApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepositController : ControllerBase
+    {
+        private readonly DapperService _dapperService;
+
+        public DepositController(DapperService dapperService)
+        {
+            _dapperService = dapperService;
+        }
+
+        #region GetDeposits List
+
+        [HttpGet]
+        public IActionResult GetDeposits()
+        {
+            string query = "SELECT * FROM Tbl_Deposit WHERE DeleteFlag = 0;";
+            List<DepositModel> lst = _dapperService.Query<DepositModel>(query).ToList();
+            return Ok(lst);
+        }
+
+        #endregion
+    }
+}
