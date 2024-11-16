943c96752e77390fc2c2153d08571d5a56c561f0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 09:56:08 +0630

    Feat : Add UserResponseModel

---
+++ MTKDotNetCore.MiniKpayDapperApi/Models/User/UserResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.User
+{
+    public class UserResponseModel
+    {
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal Balance { get; set; }
+        public string Pin { get; set; }
+        public string Password { get; set; }
+        public bool DeleteFlag { get; set; }
+    }
+}
