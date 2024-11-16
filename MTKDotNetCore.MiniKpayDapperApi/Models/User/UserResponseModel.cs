9b622ff1c6982dc3a7ea166165628ef049e450ea
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 15:13:15 +0630

    Format Code

--- MTKDotNetCore.MiniKpayDapperApi/Models/User/UserResponseModel.cs
+++ MTKDotNetCore.MiniKpayDapperApi/Models/User/UserResponseModel.cs
@@ -1,11 +1,10 @@
-﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.User
+﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.User;
+
+public class UserResponseModel
 {
-    public class UserResponseModel
-    {
-        public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
-        public decimal Balance { get; set; }
-        public string Pin { get; set; }
-        public string Password { get; set; }
-    }
+    public string FullName { get; set; }
+    public string PhoneNumber { get; set; }
+    public decimal Balance { get; set; }
+    public string Pin { get; set; }
+    public string Password { get; set; }
 }
