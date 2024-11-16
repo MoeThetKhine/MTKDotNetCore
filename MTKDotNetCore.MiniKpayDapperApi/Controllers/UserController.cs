130a35bdc78d35775f945db413d33fcb296232f1
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 10:26:13 +0630

    Feat : Add Query in DapperService

---
+++ MTKDotNetCore.MiniKpayDapperApi/Controllers/UserController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MTKDotNetCore.MiniKpayDapperApi.Controllers
+{
+    [Route("api/[user]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly string _connectionString = "Data Source=.;Initial Catalog=MiniKpay;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
+    }
+}
---
+++ MTKDotNetCore.MiniKpayDapperApi/DapperService.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MTKDotNetCore.MiniKpayDapperApi
+{
+    public class DapperService
+    {
+        private readonly string _connectionString;
+
+        public DapperService(string conectionString)
+        {
+            _connectionString = conectionString;
+        }
+        #region Query
+
+        public List<T> Query<T>(string query, object? param = null)
+        {
+            using IDbConnection db = new SqlConnection(_connectionString);
+
+            var lst = db.Query<T>(query, param).ToList();
+            return lst;
+        }
+
+        #endregion
+    }
+}
