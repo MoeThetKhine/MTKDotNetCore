d0fb4bcc1bfd1fb4fa4328596bd1b4194336a283
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 15:14:05 +0630

    Format Code

--- MTKDotNetCore.MiniKpayDapperApi/Models/Transaction/TransactionModel.cs
+++ MTKDotNetCore.MiniKpayDapperApi/Models/Transaction/TransactionModel.cs
@@ -1,13 +1,12 @@
-﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.Transaction
-{
-    public class TransactionModel
-    {
-        public int Transaction_Id {  get; set; }
-        public string FromPhoneNumber { get; set; }
-        public string ToPhoneNumber { get; set; }
-        public decimal Amount { get; set; }
-        public string Pin { get; set; }
-        public bool DeleteFlag {  get; set; }
+﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.Transaction;
 
-    }
+public class TransactionModel
+{
+    public int Transaction_Id {  get; set; }
+    public string FromPhoneNumber { get; set; }
+    public string ToPhoneNumber { get; set; }
+    public decimal Amount { get; set; }
+    public string Pin { get; set; }
+    public bool DeleteFlag {  get; set; }
+
 }
