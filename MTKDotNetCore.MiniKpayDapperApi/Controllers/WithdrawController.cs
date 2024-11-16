38f04b9cde93dc840fbf2ccb3e7004fbf0ce803d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 12:05:28 +0630

    Feat : Add Get Withdraw List

---
+++ MTKDotNetCore.MiniKpayDapperApi/Controllers/WithdrawController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MTKDotNetCore.MiniKpayDapperApi.Models.Deposit;
+using MTKDotNetCore.MiniKpayDapperApi.Models.Withdraw;
+
+namespace MTKDotNetCore.MiniKpayDapperApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WithdrawController : ControllerBase
+    {
+        private readonly DapperService _dapperService;
+
+        public WithdrawController(DapperService dapperService)
+        {
+            _dapperService = dapperService;
+        }
+
+        #region Get Withdraw List
+
+        [HttpGet]
+        public IActionResult GetWithdraw()
+        {
+            string query = "SELECT * FROM Tbl_Withdraw WHERE DeleteFlag = 0;";
+            List<WithdrawModel> lst = _dapperService.Query<WithdrawModel>(query).ToList();
+            return Ok(lst);
+        }
+
+        #endregion
+
+    }
+}
