723ee4cd6d95ed00058b896a616b07afad005430
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 09:55:48 +0630

    Feat: Add UserModel

---
+++ MTKDotNetCore.MiniKpayDapperApi/Models/User/UserModel.cs
@@ -0,0 +1,13 @@
+﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.User
+{
+    public class UserModel
+    {
+        public int User_Id { get; set; }
+        public string FullName { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal Balance { get; set; }
+        public string Pin { get; set; }
+        public string Password { get; set; }
+        public bool DeleteFlag { get; set; }
+    }
+}
