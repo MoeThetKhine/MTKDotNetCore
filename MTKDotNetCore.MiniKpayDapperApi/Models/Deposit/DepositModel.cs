efe63384d94cc13697f52241f23ca76745919080
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 09:57:22 +0630

    Feat: Add DepositModel

---
+++ MTKDotNetCore.MiniKpayDapperApi/Models/Deposit/DepositModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.Deposit
+{
+    public class DepositModel
+    {
+        public int Deposit_Id { get; set; }
+        public string PhoneNumber { get; set; }
+        public decimal Balance { get; set; }
+        public bool DeleteFlag { get; set; }
+    }
+}
