ceb22babd5f69cd456295e09df87444d7e3348bf
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-16 15:13:47 +0630

    Format Code

--- MTKDotNetCore.MiniKpayDapperApi/Models/Transaction/TransactionResponseModel.cs
+++ MTKDotNetCore.MiniKpayDapperApi/Models/Transaction/TransactionResponseModel.cs
@@ -1,10 +1,9 @@
-﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.Transaction
+﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.Transaction;
+
+public class TransactionResponseModel
 {
-    public class TransactionResponseModel
-    {
-        public string FromPhoneNumber { get; set; }
-        public string ToPhoneNumber { get; set; }
-        public decimal Amount { get; set; }
-        public string Pin { get; set; }
-    }
+    public string FromPhoneNumber { get; set; }
+    public string ToPhoneNumber { get; set; }
+    public decimal Amount { get; set; }
+    public string Pin { get; set; }
 }
