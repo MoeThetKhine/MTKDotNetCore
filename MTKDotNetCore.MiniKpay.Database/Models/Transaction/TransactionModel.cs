2e7610ffb98d20f1569e8fd97962b75aedfe253f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-17 11:26:59 +0630

    Feat : Add TransactionModel

---
+++ MTKDotNetCore.MiniKpay.Database/Models/Transaction/TransactionModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MTKDotNetCore.MiniKpay.Database.Models.Transaction;
+
+public class TransactionModel
+{
+    public int Transaction_Id { get; set; }
+    public string FromPhoneNumber { get; set; }
+    public string ToPhoneNumber { get; set; }
+    public decimal Amount { get; set; }
+    public string Pin { get; set; }
+    public bool DeleteFlag { get; set; }
+
+}
