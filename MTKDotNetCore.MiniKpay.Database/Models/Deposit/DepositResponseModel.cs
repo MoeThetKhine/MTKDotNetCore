e689fa059f93916cb25fa0d38c546fffcc035bb2
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-17 11:22:52 +0630

    Feat : Add DepositResponseModel

---
+++ MTKDotNetCore.MiniKpay.Database/Models/Deposit/DepositResponseModel.cs
@@ -0,0 +1,7 @@
+﻿namespace MTKDotNetCore.MiniKpay.Database.Models.Deposit;
+
+public class DepositResponseModel
+{
+    public string PhoneNumber { get; set; }
+    public decimal Balance { get; set; }
+}
