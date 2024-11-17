ccfbf21d0700d272814b4007076fae992ed49c6c
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-17 11:22:07 +0630

    Feat : Add Deposit Model

---
+++ MTKDotNetCore.MiniKpay.Database/Models/Deposit/DepositModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MTKDotNetCore.MiniKpay.Database.Models.Deposit;
+
+public class DepositModel
+{
+    public int Deposit_Id { get; set; }
+    public string PhoneNumber { get; set; }
+    public decimal Balance { get; set; }
+    public bool DeleteFlag { get; set; }
+}
