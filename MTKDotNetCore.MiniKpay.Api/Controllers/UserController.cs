79f8ed949e811093b687c17dd728231a43bef8b6
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-17 16:18:40 +0630

    Feat : Add Global Using

--- MTKDotNetCore.MiniKpay.Api/Controllers/UserController.cs
+++ MTKDotNetCore.MiniKpay.Api/Controllers/UserController.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using MTKDotNetCore.MiniKpay.Database.Models.User;
-using MTKDotNetCore.MiniKpay.Domain.BusinessLogic.User;
-
-namespace MTKDotNetCore.MiniKpay.Api.Controllers;
+﻿namespace MTKDotNetCore.MiniKpay.Api.Controllers;
 
 [Route("api/[controller]")]
 [ApiController]
---
+++ MTKDotNetCore.MiniKpay.Api/GlobalUsing.cs
@@ -0,0 +1,3 @@
+﻿global using Microsoft.AspNetCore.Mvc;
+global using MTKDotNetCore.MiniKpay.Database.Models.User;
+global using MTKDotNetCore.MiniKpay.Domain.BusinessLogic.User;
