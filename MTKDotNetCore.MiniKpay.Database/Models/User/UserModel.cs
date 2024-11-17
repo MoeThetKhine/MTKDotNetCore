74870a95784c73695ba73d7cde383ee307b645b3
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-17 11:20:40 +0630

    Feat : Add UserModel

---
+++ MTKDotNetCore.MiniKpay.Database/Models/User/UserModel.cs
@@ -0,0 +1,12 @@
+﻿namespace MTKDotNetCore.MiniKpay.Database.Models.User;
+
+public class UserModel
+{
+    public int User_Id { get; set; }
+    public string FullName { get; set; }
+    public string PhoneNumber { get; set; }
+    public decimal Balance { get; set; }
+    public string Pin { get; set; }
+    public string Password { get; set; }
+    public bool DeleteFlag { get; set; }
+}
