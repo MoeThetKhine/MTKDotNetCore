c0988aa9a1952ce57d1f9fce8262f2f800e644d5
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-17 11:28:56 +0630

    Feat : Add DataAccess_Transaction

--- MTKDotNetCore.MiniKpay.Database/Class1.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MTKDotNetCore.MiniKpay.Database
-{
-    public class Class1
-    {
-
-    }
-}
---
+++ MTKDotNetCore.MiniKpay.Database/DataAccess/Transaction/DataAccess_Transaction.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKDotNetCore.MiniKpay.Database.DataAccess.Transaction
+{
+    internal class DataAccess_Transaction
+    {
+    }
+}
