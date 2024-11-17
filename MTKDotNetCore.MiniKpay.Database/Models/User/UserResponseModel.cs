f0ba994a2a5f07f1a01c687d9fd8bc1caede7c47
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-17 11:21:45 +0630

    Feat : Add UserResponseModel

---
+++ MTKDotNetCore.MiniKpay.Database/Models/User/UserResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MTKDotNetCore.MiniKpay.Database.Models.User;
+
+public class UserResponseModel
+{
+    public string FullName { get; set; }
+    public string PhoneNumber { get; set; }
+    public decimal Balance { get; set; }
+    public string Pin { get; set; }
+    public string Password { get; set; }
+}
