4c65b8802c42edfe9db4706c2281ef6c6813e9bc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-17 11:23:49 +0630

    Feat : Add WithdrawModel

---
+++ MTKDotNetCore.MiniKpay.Database/Models/Withdraw/WithdrawModel.cs
@@ -0,0 +1,9 @@
+﻿namespace MTKDotNetCore.MiniKpay.Database.Models.Withdraw;
+
+public class WithdrawModel
+{
+    public int Withdraw_Id { get; set; }
+    public string PhoneNumber { get; set; }
+    public decimal Balance { get; set; }
+    public bool DeleteFlag { get; set; }
+}
