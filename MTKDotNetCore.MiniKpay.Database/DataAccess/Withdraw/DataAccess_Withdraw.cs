a2bc08f142c3aca34364e6beff1b2340752cf163
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-17 11:28:02 +0630

    Feat : Add DataAccess_Withdraw

---
+++ MTKDotNetCore.MiniKpay.Database/DataAccess/Withdraw/DataAccess_Withdraw.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKDotNetCore.MiniKpay.Database.DataAccess.Withdraw
+{
+    internal class DataAccess_Withdraw
+    {
+    }
+}
