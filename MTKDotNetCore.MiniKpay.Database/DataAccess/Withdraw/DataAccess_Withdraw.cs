3528ca5611ccf37e334368a2b94f66949103fa2f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-20 23:56:36 +0630

    Feat: Add GetWithdrawListAsync

--- MTKDotNetCore.MiniKpay.Database/DataAccess/Withdraw/DataAccess_Withdraw.cs
+++ MTKDotNetCore.MiniKpay.Database/DataAccess/Withdraw/DataAccess_Withdraw.cs
@@ -1,6 +1,26 @@
-﻿namespace MTKDotNetCore.MiniKpay.Database.DataAccess.Withdraw;
+﻿using MTKDotNetCore.MiniKpay.Database.Models.Withdraw;
+
+namespace MTKDotNetCore.MiniKpay.Database.DataAccess.Withdraw;
 
 public class DataAccess_Withdraw
 {
+    private readonly DapperService _dapperService;
+
+    public DataAccess_Withdraw(DapperService dapperService)
+    {
+        _dapperService = dapperService;
+    }
+
+    #region GetWithdrawListAsync
+
+    public async Task<List<WithdrawModel>> GetWithdrawListAsync()
+    {
+        string query = "SELECT * FROM Tbl_Withdraw WHERE DeleteFlag =0;";
+        var result = await _dapperService.QueryAsync<WithdrawModel>(query); 
+        return result.ToList();
+    }
+
+    #endregion
+
 
 }
