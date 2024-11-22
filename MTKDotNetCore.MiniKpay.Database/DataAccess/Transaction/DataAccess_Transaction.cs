49f1e63e5984c0ae54cff44981410338ec6f5c1d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-22 11:48:46 +0630

    Feat : Add Get Transaction

--- MTKDotNetCore.MiniKpay.Database/DataAccess/Transaction/DataAccess_Transaction.cs
+++ MTKDotNetCore.MiniKpay.Database/DataAccess/Transaction/DataAccess_Transaction.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MTKDotNetCore.MiniKpay.Database.Models.Transaction;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace MTKDotNetCore.MiniKpay.Database.DataAccess.Transaction
 {
-    internal class DataAccess_Transaction
+    public class DataAccess_Transaction
     {
+        private readonly DapperService _dapperService;
+
+        public DataAccess_Transaction(DapperService dapperService)
+        {
+            _dapperService = dapperService;
+        }
+        #region Get All Transactions
+
+        public async Task<List<TransactionModel>> GetTransactionListAsync()
+        {
+            string query = "SELECT * FROM Tbl_Transaction WHERE DeleteFlag = 0;";
+            var result = await _dapperService.QueryAsync<TransactionModel>(query);
+            return result.ToList();
+        }
+
+        #endregion
+
     }
 }
