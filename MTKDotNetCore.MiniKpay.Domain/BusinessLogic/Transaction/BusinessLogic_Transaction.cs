897975a771c4e4dc485ff90b267eb17ff33efd4c
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-22 22:45:19 +0630

    Feat : Add Get Transaction

--- MTKDotNetCore.MiniKpay.Domain/BusinessLogic/Transaction/BusinessLogic_Transaction.cs
+++ MTKDotNetCore.MiniKpay.Domain/BusinessLogic/Transaction/BusinessLogic_Transaction.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using MTKDotNetCore.MiniKpay.Database.DataAccess.Transaction;
+using MTKDotNetCore.MiniKpay.Database.Models.Transaction;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,20 @@ namespace MTKDotNetCore.MiniKpay.Domain.BusinessLogic.Transaction
 {
     public class BusinessLogic_Transaction
     {
+        private readonly DataAccess_Transaction _dA_Transaction;
+
+        public BusinessLogic_Transaction(DataAccess_Transaction dataAccessTransaction)
+        {
+            _dA_Transaction = dataAccessTransaction;
+        }
+
+        #region Get  Transaction
+
+        public async Task<List<TransactionModel>> GetTransactionAsync()
+        {
+            return await _dA_Transaction.GetTransactionListAsync();
+        }
+
+        #endregion
     }
 }
