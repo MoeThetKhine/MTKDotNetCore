087674e137d27a0c1e80fa6df141230f94400b8d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-22 23:44:36 +0630

    Feat : Add Get Transcation

---
+++ MTKDotNetCore.MiniKpay.Api/Controllers/TransactionController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MTKDotNetCore.MiniKpay.Domain.BusinessLogic.Transaction;
+
+namespace MTKDotNetCore.MiniKpay.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TransactionController : ControllerBase
+    {
+        private readonly BusinessLogic_Transaction _bL_Transaction;
+
+        public TransactionController(BusinessLogic_Transaction businessLogicTransaction)
+        {
+            _bL_Transaction = businessLogicTransaction;
+        }
+
+        #region GetTransactions
+
+        [HttpGet]
+        public async Task<IActionResult> GetTransactions()
+        {
+            var transaction = await _bL_Transaction.GetTransactionAsync();
+            if(transaction is null)
+            {
+                return NotFound("No transaction found");
+            }
+            return Ok(transaction);
+        }
+
+        #endregion
+    }
+}
