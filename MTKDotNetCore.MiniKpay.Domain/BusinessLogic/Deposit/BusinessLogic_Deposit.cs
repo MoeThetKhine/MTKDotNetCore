dd8058cae4eaa81f40395116e931a2f2743cd09f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-22 10:40:15 +0630

    Feat : Add Get Deposit List Async

--- MTKDotNetCore.MiniKpay.Domain/BusinessLogic/Deposit/BusinessLogic_Deposit.cs
+++ MTKDotNetCore.MiniKpay.Domain/BusinessLogic/Deposit/BusinessLogic_Deposit.cs
@@ -1,12 +1,25 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MTKDotNetCore.MiniKpay.Database.DataAccess.Deposit;
+using MTKDotNetCore.MiniKpay.Database.Models.Deposit;
 
 namespace MTKDotNetCore.MiniKpay.Domain.BusinessLogic.Deposit
 {
     public class BusinessLogic_Deposit
     {
+        private readonly DataAccess_Deposit _dA_Deposit;
+
+        public BusinessLogic_Deposit(DataAccess_Deposit dA_Deposit)
+        {
+            _dA_Deposit = dA_Deposit;
+        }
+
+        #region Get Deposit List Async
+
+        public async Task<List<DepositModel>> GetDepositListAsync()
+        {
+            var result = await _dA_Deposit.GetDepositListAsync();
+            return result;
+        }
+
+        #endregion
     }
 }
