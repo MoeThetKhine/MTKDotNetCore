53ded4c5cf1f365c9d84473d741a428ecd7eb51a
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-22 22:24:20 +0630

    Format Code

--- MTKDotNetCore.MiniKpay.Database/DataAccess/Transaction/DataAccess_Transaction.cs
+++ MTKDotNetCore.MiniKpay.Database/DataAccess/Transaction/DataAccess_Transaction.cs
@@ -1,65 +1,64 @@
-﻿namespace MTKDotNetCore.MiniKpay.Database.DataAccess.Transaction
+﻿namespace MTKDotNetCore.MiniKpay.Database.DataAccess.Transaction;
+
+public class DataAccess_Transaction
 {
-    public class DataAccess_Transaction
+    private readonly DapperService _dapperService;
+
+    public DataAccess_Transaction(DapperService dapperService)
     {
-        private readonly DapperService _dapperService;
+        _dapperService = dapperService;
+    }
+    #region Get All Transactions
 
-        public DataAccess_Transaction(DapperService dapperService)
-        {
-            _dapperService = dapperService;
-        }
-        #region Get All Transactions
+    public async Task<List<TransactionModel>> GetTransactionListAsync()
+    {
+        string query = "SELECT * FROM Tbl_Transaction WHERE DeleteFlag = 0;";
+        var result = await _dapperService.QueryAsync<TransactionModel>(query);
+        return result.ToList();
+    }
 
-        public async Task<List<TransactionModel>> GetTransactionListAsync()
-        {
-            string query = "SELECT * FROM Tbl_Transaction WHERE DeleteFlag = 0;";
-            var result = await _dapperService.QueryAsync<TransactionModel>(query);
-            return result.ToList();
-        }
+    #endregion
 
-        #endregion
+    #region Get Transaction By From Phone Number
 
-        #region Get Transaction By From Phone Number
+    public async Task<List<TransactionModel>> GetTransactionByFromPhoneNumberAsync(string fromPhoneNumber)
+    {
+        string query = "SELECT * FROM Tbl_Transaction WHERE FromPhoneNumber = @FromPhoneNumber AND DeleteFlag = 0;";
+        var result = await _dapperService.QueryAsync<TransactionModel>(query, new { fromPhoneNumber = fromPhoneNumber });
+        return result.ToList();
+    }
 
-        public async Task<List<TransactionModel>> GetTransactionByFromPhoneNumberAsync(string fromPhoneNumber)
-        {
-            string query = "SELECT * FROM Tbl_Transaction WHERE FromPhoneNumber = @FromPhoneNumber AND DeleteFlag = 0;";
-            var result = await _dapperService.QueryAsync<TransactionModel>(query, new { fromPhoneNumber = fromPhoneNumber });
-            return result.ToList();
-        }
+    #endregion
 
-        #endregion
+    #region Get User By Phone Number 
 
-        #region Get User By Phone Number 
+    public async Task<UserModel> GetUserByPhoneNumberAsync(string phoneNumber)
+    {
+        string query = "SELECT * FROM Tbl_User WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
+        return await _dapperService.QueryFirstOrDefaultAsync<UserModel>(query, new { phoneNumber = phoneNumber });
+    }
 
-        public async Task<UserModel> GetUserByPhoneNumberAsync(string phoneNumber)
-        {
-            string query = "SELECT * FROM Tbl_User WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
-            return await _dapperService.QueryFirstOrDefaultAsync<UserModel>(query, new { phoneNumber = phoneNumber });
-        }
+    #endregion
 
-        #endregion
+    #region Update User Balance 
 
-        #region Update User Balance 
+    public async Task<int> UpdateUserBalanceAsync(string phoneNumber,decimal updateBalance)
+    {
+        string query = "UPDATE Tbl_User SET Balance = @UpdatedBalance WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
+        return await _dapperService.ExecuteAsync(query,new {UpdatedBalance = updateBalance, PhoneNumber = phoneNumber});
+    }
 
-        public async Task<int> UpdateUserBalanceAsync(string phoneNumber,decimal updateBalance)
-        {
-            string query = "UPDATE Tbl_User SET Balance = @UpdatedBalance WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
-            return await _dapperService.ExecuteAsync(query,new {UpdatedBalance = updateBalance, PhoneNumber = phoneNumber});
-        }
+    #endregion
 
-        #endregion
+    #region Create Transaction
 
-        #region Create Transaction
-
-        public async Task<int> CreateTransactionAsync(TransactionResponseModel transaction)
-        {
-            string query = @"INSERT INTO Tbl_Transaction 
+    public async Task<int> CreateTransactionAsync(TransactionResponseModel transaction)
+    {
+        string query = @"INSERT INTO Tbl_Transaction 
                              (FromPhoneNumber, ToPhoneNumber, Amount, Pin, DeleteFlag) 
                              VALUES (@FromPhoneNumber, @ToPhoneNumber, @Amount, @Pin, 0);";
-            return await _dapperService.ExecuteAsync(query, transaction);
-        }
-
-        #endregion
+        return await _dapperService.ExecuteAsync(query, transaction);
     }
+
+    #endregion
 }
