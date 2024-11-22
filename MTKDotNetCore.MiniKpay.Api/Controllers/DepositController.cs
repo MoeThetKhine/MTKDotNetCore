150cd17e9269bdd10b0d48c77e71c12931d15f9b
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-22 11:30:11 +0630

    Format Code

--- MTKDotNetCore.MiniKpay.Api/Controllers/DepositController.cs
+++ MTKDotNetCore.MiniKpay.Api/Controllers/DepositController.cs
@@ -1,67 +1,66 @@
-﻿namespace MTKDotNetCore.MiniKpay.Api.Controllers
+﻿namespace MTKDotNetCore.MiniKpay.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class DepositController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class DepositController : ControllerBase
+    private readonly BusinessLogic_Deposit _bL_Deposit;
+
+    public DepositController(BusinessLogic_Deposit businessLogic_Deposit)
     {
-        private readonly BusinessLogic_Deposit _bL_Deposit;
-
-        public DepositController(BusinessLogic_Deposit businessLogic_Deposit)
-        {
-            _bL_Deposit = businessLogic_Deposit;
-        }
-
-        #region Get Deposit List
-
-        [HttpGet]
-        public async Task<IActionResult> GetDeposits()
-        {
-            var deposits = await _bL_Deposit.GetDepositListAsync();
-            if (deposits is null)
-            {
-                return NotFound("No deposits found.");
-            }
-
-            return Ok(deposits);
-        }
-
-        #endregion
-
-        #region Get Deposit By PhoneNumber
-
-        [HttpGet("{phoneNumber}")]
-        public async Task<IActionResult> GetDepositByPhoneNumber(string phoneNumber)
-        {
-            var deposits = await _bL_Deposit.GetDepositByPhoneNumberAsync(phoneNumber);
-            if (deposits is null)
-            {
-                return NotFound("No deposits found for phone number.");
-            }
-
-            return Ok(deposits);
-        }
-
-        #endregion
-
-        #region Create Deposit
-
-        [HttpPost]
-        public async Task<IActionResult> CreateDeposit([FromBody] DepositResponseModel deposit)
-        {
-            if (deposit is null)
-            {
-                return BadRequest("Invalid deposit request.");
-            }
-
-            var result = await _bL_Deposit.CreateDepositAsync(deposit);
-            
-            if(result is null)
-            {
-                return BadRequest("Deposit Failed");
-            }
-            return Ok("Deposit process completed successfully");
-        }
-
-        #endregion
+        _bL_Deposit = businessLogic_Deposit;
     }
+
+    #region Get Deposit List
+
+    [HttpGet]
+    public async Task<IActionResult> GetDeposits()
+    {
+        var deposits = await _bL_Deposit.GetDepositListAsync();
+        if (deposits is null)
+        {
+            return NotFound("No deposits found.");
+        }
+
+        return Ok(deposits);
+    }
+
+    #endregion
+
+    #region Get Deposit By PhoneNumber
+
+    [HttpGet("{phoneNumber}")]
+    public async Task<IActionResult> GetDepositByPhoneNumber(string phoneNumber)
+    {
+        var deposits = await _bL_Deposit.GetDepositByPhoneNumberAsync(phoneNumber);
+        if (deposits is null)
+        {
+            return NotFound("No deposits found for phone number.");
+        }
+
+        return Ok(deposits);
+    }
+
+    #endregion
+
+    #region Create Deposit
+
+    [HttpPost]
+    public async Task<IActionResult> CreateDeposit([FromBody] DepositResponseModel deposit)
+    {
+        if (deposit is null)
+        {
+            return BadRequest("Invalid deposit request.");
+        }
+
+        var result = await _bL_Deposit.CreateDepositAsync(deposit);
+        
+        if(result is null)
+        {
+            return BadRequest("Deposit Failed");
+        }
+        return Ok("Deposit process completed successfully");
+    }
+
+    #endregion
 }
