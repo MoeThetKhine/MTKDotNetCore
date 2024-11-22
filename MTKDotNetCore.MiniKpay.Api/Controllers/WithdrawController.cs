88d5efa3ee2aa08a7f38a809a980f7326d77b3e5
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-22 09:39:04 +0630

    Feat : Add Global Using

--- MTKDotNetCore.MiniKpay.Api/Controllers/WithdrawController.cs
+++ MTKDotNetCore.MiniKpay.Api/Controllers/WithdrawController.cs
@@ -1,9 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
-using MTKDotNetCore.MiniKpay.Database.Models.Withdraw;
-using MTKDotNetCore.MiniKpay.Domain.BusinessLogic.Withdraw;
-
-namespace MTKDotNetCore.MiniKpay.Api.Controllers
+﻿namespace MTKDotNetCore.MiniKpay.Api.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
--- MTKDotNetCore.MiniKpay.Api/GlobalUsing.cs
+++ MTKDotNetCore.MiniKpay.Api/GlobalUsing.cs
@@ -3,3 +3,5 @@ global using MTKDotNetCore.MiniKpay.Database.Models.User;
 global using MTKDotNetCore.MiniKpay.Domain.BusinessLogic.User;
 global using MTKDotNetCore.MiniKpay.Database;
 global using MTKDotNetCore.MiniKpay.Database.DataAccess.User;
+global using MTKDotNetCore.MiniKpay.Database.Models.Withdraw;
+global using MTKDotNetCore.MiniKpay.Domain.BusinessLogic.Withdraw;
