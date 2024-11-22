17935409d75312f6d1537dd24e68e2c30b661d24
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-22 23:25:48 +0630

    Feat : Add Create Transaction

--- MTKDotNetCore.MiniKpay.Domain/BusinessLogic/Transaction/BusinessLogic_Transaction.cs
+++ MTKDotNetCore.MiniKpay.Domain/BusinessLogic/Transaction/BusinessLogic_Transaction.cs
@@ -34,7 +34,63 @@ namespace MTKDotNetCore.MiniKpay.Domain.BusinessLogic.Transaction
         }
 
         #endregion
+        #region Create Transaction
 
+        public async Task<string> CreateTransactionAsync(TransactionResponseModel transaction)
+        {
+            if (transaction.Amount <= 0)
+            {
+                return "Transaction amount must be greater than 0.";
+            }
 
+            if (transaction.FromPhoneNumber == transaction.ToPhoneNumber)
+            {
+                return "Sender and receiver phone numbers must be different.";
+            }
+
+            var sender = await _dA_Transaction.GetUserByPhoneNumberAsync(transaction.FromPhoneNumber);
+            if (sender is null)
+            {
+                return "Sender phone number does not exist.";
+            }
+
+            if (sender.Pin != transaction.Pin)
+            {
+                return "Invalid PIN for the sender.";
+            }
+
+            var receiver = await _dA_Transaction.GetUserByPhoneNumberAsync(transaction.ToPhoneNumber);
+            if (receiver is null)
+            {
+                return "Receiver phone number does not exist.";
+            }
+
+            if (sender.Balance < transaction.Amount)
+            {
+                return "Sender has insufficient balance.";
+            }
+
+            var updatedSenderBalance = sender.Balance - transaction.Amount;
+            var updatedReceiverBalance = receiver.Balance + transaction.Amount;
+
+            int updateSenderResult = await _dA_Transaction.UpdateUserBalanceAsync(transaction.FromPhoneNumber, updatedSenderBalance);
+            if (updateSenderResult == 0)
+            {
+                return "Failed to update sender's balance.";
+            }
+
+            int updateReceiverResult = await _dA_Transaction.UpdateUserBalanceAsync(transaction.ToPhoneNumber, updatedReceiverBalance);
+            if (updateReceiverResult == 0)
+            {
+                return "Failed to update receiver's balance.";
+            }
+
+            int insertResult = await _dA_Transaction.CreateTransactionAsync(transaction);
+            return insertResult > 0 ? "Transaction completed successfully." : "Transaction process failed.";
+        }
+
+        #endregion
     }
+
 }
+
