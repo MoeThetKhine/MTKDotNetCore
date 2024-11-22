baae58def0c94dad493f2e15693db91aab843a10
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-22 10:05:51 +0630

    Feat : Add Get Deposit List

--- MTKDotNetCore.MiniKpay.Database/DataAccess/Deposit/DataAccess_Deposit.cs
+++ MTKDotNetCore.MiniKpay.Database/DataAccess/Deposit/DataAccess_Deposit.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MTKDotNetCore.MiniKpay.Database.Models.Deposit;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,24 @@ using System.Threading.Tasks;
 
 namespace MTKDotNetCore.MiniKpay.Database.DataAccess.Deposit
 {
-    internal class DataAccess_Deposit
+    public class DataAccess_Deposit
     {
+        private readonly DapperService _dapperService;
+
+        public DataAccess_Deposit(DapperService dapperService)
+        {
+            _dapperService = dapperService;
+        }
+
+        #region Get Deposit List 
+
+        public async Task<List<DepositModel>> GetDepositListAsync()
+        {
+            string query = "SELECT * FROM Tbl_Deposit WHERE DeleteFlag = 0;";
+            var result = await _dapperService.QueryAsync<DepositModel>(query); 
+            return result.ToList();
+        }
+
+        #endregion
     }
 }
