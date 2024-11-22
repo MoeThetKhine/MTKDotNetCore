bc608c58221043653693fa27bbb7dd9609ecd416
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-22 10:35:23 +0630

    Format Code

--- MTKDotNetCore.MiniKpay.Database/DataAccess/Deposit/DataAccess_Deposit.cs
+++ MTKDotNetCore.MiniKpay.Database/DataAccess/Deposit/DataAccess_Deposit.cs
@@ -1,66 +1,63 @@
-﻿
-namespace MTKDotNetCore.MiniKpay.Database.DataAccess.Deposit
+﻿namespace MTKDotNetCore.MiniKpay.Database.DataAccess.Deposit;
+
+public class DataAccess_Deposit
 {
-    public class DataAccess_Deposit
+    private readonly DapperService _dapperService;
+
+    public DataAccess_Deposit(DapperService dapperService)
     {
-        private readonly DapperService _dapperService;
-
-        public DataAccess_Deposit(DapperService dapperService)
-        {
-            _dapperService = dapperService;
-        }
-
-        #region Get Deposit List 
-
-        public async Task<List<DepositModel>> GetDepositListAsync()
-        {
-            string query = "SELECT * FROM Tbl_Deposit WHERE DeleteFlag = 0;";
-            var result = await _dapperService.QueryAsync<DepositModel>(query); 
-            return result.ToList();
-        }
-
-        #endregion
-
-        #region Get Deposit By PhoneNumber Async
-
-        public async Task<List<DepositModel>> GetDepositByPhoneNumberAsync(string phoneNumber)
-        {
-            string query = "SELECT * FROM Tbl_Deposit WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
-            var result = await _dapperService.QueryAsync<DepositModel>(query, new { PhoneNumber = phoneNumber });
-            return result.ToList();
-        }
-
-        #endregion
-
-        #region Get User By PhoneNumberAsync
-
-        public async Task<UserModel> GetUserByPhoneNumberAsync(string phoneNumber)
-        {
-            string query = "SELECT * FROM Tbl_User WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
-            return await _dapperService.QueryFirstOrDefaultAsync<UserModel>(query, new { PhoneNumber = phoneNumber });
-        }
-
-        #endregion
-
-        #region Update User Balance
-
-        public async Task<int> UpdateUserBalanceAsync(string phoneNumber, decimal updateBalance)
-        {
-            string query = "UPDATE Tbl_User SET Balance = @UpdatedBalance WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
-            return await _dapperService.ExecuteAsync(query, new {UpdatedBalance = updateBalance, PhoneNumber = phoneNumber});
-        }
-
-        #endregion
-
-        #region Create Deposit 
-
-        public async Task<int> CreateDepositAsync(DepositResponseModel deposit)
-        {
-            string query = @"INSERT INTO Tbl_Deposit (PhoneNumber, Balance, DeleteFlag) VALUES (@PhoneNumber, @Balance, 0);";
-            return await _dapperService.ExecuteAsync(query, deposit);
-        }
-
-        #endregion
+        _dapperService = dapperService;
     }
+
+    #region Get Deposit List 
+
+    public async Task<List<DepositModel>> GetDepositListAsync()
+    {
+        string query = "SELECT * FROM Tbl_Deposit WHERE DeleteFlag = 0;";
+        var result = await _dapperService.QueryAsync<DepositModel>(query); 
+        return result.ToList();
+    }
+
+    #endregion
+
+    #region Get Deposit By PhoneNumber Async
+
+    public async Task<List<DepositModel>> GetDepositByPhoneNumberAsync(string phoneNumber)
+    {
+        string query = "SELECT * FROM Tbl_Deposit WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
+        var result = await _dapperService.QueryAsync<DepositModel>(query, new { PhoneNumber = phoneNumber });
+        return result.ToList();
+    }
+
+    #endregion
+
+    #region Get User By PhoneNumberAsync
+
+    public async Task<UserModel> GetUserByPhoneNumberAsync(string phoneNumber)
+    {
+        string query = "SELECT * FROM Tbl_User WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
+        return await _dapperService.QueryFirstOrDefaultAsync<UserModel>(query, new { PhoneNumber = phoneNumber });
+    }
+
+    #endregion
+
+    #region Update User Balance
+
+    public async Task<int> UpdateUserBalanceAsync(string phoneNumber, decimal updateBalance)
+    {
+        string query = "UPDATE Tbl_User SET Balance = @UpdatedBalance WHERE PhoneNumber = @PhoneNumber AND DeleteFlag = 0;";
+        return await _dapperService.ExecuteAsync(query, new {UpdatedBalance = updateBalance, PhoneNumber = phoneNumber});
+    }
+
+    #endregion
+
+    #region Create Deposit 
+
+    public async Task<int> CreateDepositAsync(DepositResponseModel deposit)
+    {
+        string query = @"INSERT INTO Tbl_Deposit (PhoneNumber, Balance, DeleteFlag) VALUES (@PhoneNumber, @Balance, 0);";
+        return await _dapperService.ExecuteAsync(query, deposit);
+    }
+
+    #endregion
 }
-    
