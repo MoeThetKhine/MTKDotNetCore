a77fa9817065284f165431b44b14b0102f161db6
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-22 09:32:11 +0630

    Feat : Add Get Withdraw List in Controller

---
+++ MTKDotNetCore.MiniKpay.Api/Controllers/WithdrawController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MTKDotNetCore.MiniKpay.Domain.BusinessLogic.Withdraw;
+
+namespace MTKDotNetCore.MiniKpay.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WithdrawController : ControllerBase
+    {
+        private readonly BusinessLogic_Withdraw _bL_Withdraw;
+
+        public WithdrawController(BusinessLogic_Withdraw businessLogic_Withdraw)
+        {
+            _bL_Withdraw = businessLogic_Withdraw;
+        }
+
+        #region Get Withdraw List
+
+        [HttpGet]
+        public async Task<IActionResult> GetWithdrawList()
+        {
+            var withdrawList = await _bL_Withdraw.GetWithdrawListAsync();
+
+            if (withdrawList is null)
+            {
+                return NotFound("No withdrawal records found.");
+            }
+
+            return Ok(withdrawList);
+        }
+
+        #endregion
+    }
+}
