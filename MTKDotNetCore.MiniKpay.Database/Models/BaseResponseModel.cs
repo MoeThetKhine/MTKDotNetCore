013f74db395e25886e1ef520d84350f00a2d9a23
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-23 08:33:47 +0630

    Feat : Add BaseResponse Model

---
+++ MTKDotNetCore.MiniKpay.Database/Models/BaseResponseModel.cs
@@ -0,0 +1,24 @@
+﻿namespace MTKDotNetCore.MiniKpay.Database.Models
+{
+    public class BaseResponseModel
+    {
+        public string RespCode { get; set; }
+        public string RespDesp {  get; set; }
+        public EnumRespType RespType { get; set; }
+        public bool IsSuccess { get; set; }
+        public bool IsError { get { return !IsSuccess; } }
+
+    }
+
+    #region EnumRespType
+
+    public enum EnumRespType
+    {
+        None,
+        Success,
+        ValidationError,
+        SystemError
+    }
+
+    #endregion
+}
