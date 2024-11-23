84dfc56592a3726d0fb88cbfa9fa5465ae4ce71d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-23 09:00:30 +0630

    Format Code

--- MTKDotNetCore.MiniKpay.Api/Controllers/BaseController.cs
+++ MTKDotNetCore.MiniKpay.Api/Controllers/BaseController.cs
@@ -1,25 +1,24 @@
-﻿namespace MTKDotNetCore.MiniKpay.Api.Controllers
+﻿namespace MTKDotNetCore.MiniKpay.Api.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
+    public IActionResult Execute(object model)
     {
-        public IActionResult Execute(object model)
+        JObject jObj = JObject.Parse(JsonConvert.SerializeObject(model));
+        if(jObj.ContainsKey("Response"))
         {
-            JObject jObj = JObject.Parse(JsonConvert.SerializeObject(model));
-            if(jObj.ContainsKey("Response"))
-            {
-                BaseResponseModel baseResponseModel = JsonConvert.DeserializeObject<BaseResponseModel>(jObj["Response"]!.ToString())!;
+            BaseResponseModel baseResponseModel = JsonConvert.DeserializeObject<BaseResponseModel>(jObj["Response"]!.ToString())!;
 
-                if (baseResponseModel.RespType == EnumRespType.ValidationError)
-                    return BadRequest(model);
+            if (baseResponseModel.RespType == EnumRespType.ValidationError)
+                return BadRequest(model);
 
-                if(baseResponseModel.RespType == EnumRespType.SystemError)
-                    return BadRequest(model);
+            if(baseResponseModel.RespType == EnumRespType.SystemError)
+                return BadRequest(model);
 
-                return Ok(model);
-            }
-            return StatusCode(500, "Invalid Response Model.Please add BaseResponseModel to your ResponseModel.");
+            return Ok(model);
         }
+        return StatusCode(500, "Invalid Response Model.Please add BaseResponseModel to your ResponseModel.");
     }
 }
