80a2e26b7cf155b5d4d2cb2fab359d9eec6c0ecc
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-23 08:41:42 +0630

    Feat : Add ResponseModel

---
+++ MTKDotNetCore.MiniKpay.Database/Models/ResponseModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKDotNetCore.MiniKpay.Database.Models
+{
+    public class ResponseModel
+    {
+        public BaseResponseModel Response { get; set; }
+    }
+}
