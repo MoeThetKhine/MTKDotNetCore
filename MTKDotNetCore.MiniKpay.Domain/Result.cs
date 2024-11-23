5a0c5cd22015865f0f87f2ee9d9814688482e20d
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-23 09:19:12 +0630

    Format Code

--- MTKDotNetCore.MiniKpay.Domain/Result.cs
+++ MTKDotNetCore.MiniKpay.Domain/Result.cs
@@ -1,60 +1,58 @@
 ﻿using MTKDotNetCore.MiniKpay.Database.Models;
 
-namespace MTKDotNetCore.MiniKpay.Domain
+namespace MTKDotNetCore.MiniKpay.Domain;
+
+public class Result<T>
 {
-    public class Result<T>
+    public bool IsSuccess {  get; set; }
+    public bool IsError { get { return !IsSuccess; } }
+    public EnumRespType Type { get; set; }
+    public T Data { get; set; }
+    public string Message { get; set; }
+
+    #region Success
+
+    public static Result<T> Success<T>(T data,string message = "Success")
     {
-        public bool IsSuccess {  get; set; }
-        public bool IsError { get { return !IsSuccess; } }
-        public EnumRespType Type { get; set; }
-        public T Data { get; set; }
-        public string Message { get; set; }
-
-        #region Success
-
-        public static Result<T> Success<T>(T data,string message = "Success")
+        return new Result<T>()
         {
-            return new Result<T>()
-            {
-                IsSuccess = true,
-                Type = EnumRespType.Success,
-                Data = data,
-                Message = message
-            };
-        }
-
-        #endregion
-
-        #region ValidationError
-
-        public static Result<T> ValidationError(string message, T? data = default)
-        {
-            return new Result<T>()
-            {
-                IsSuccess = false,
-                Type = EnumRespType.ValidationError,
-                Data = data,
-                Message = message
-            };
-        }
-
-        #endregion
-
-        #region SystemError
-
-        public static Result<T> SystemError(T? data,string message)
-        {
-            return new Result<T>()
-            {
-                IsSuccess = false,
-                Type = EnumRespType.SystemError,
-                Data = data,
-                Message = message
-            };
-        }
-
-        #endregion
-
+            IsSuccess = true,
+            Type = EnumRespType.Success,
+            Data = data,
+            Message = message
+        };
     }
+
+    #endregion
+
+    #region ValidationError
+
+    public static Result<T> ValidationError(string message, T? data = default)
+    {
+        return new Result<T>()
+        {
+            IsSuccess = false,
+            Type = EnumRespType.ValidationError,
+            Data = data,
+            Message = message
+        };
+    }
+
+    #endregion
+
+    #region SystemError
+
+    public static Result<T> SystemError(T? data,string message)
+    {
+        return new Result<T>()
+        {
+            IsSuccess = false,
+            Type = EnumRespType.SystemError,
+            Data = data,
+            Message = message
+        };
+    }
+
+    #endregion
 
 }
