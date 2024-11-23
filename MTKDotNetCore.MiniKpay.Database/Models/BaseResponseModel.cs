27dba7749fd6e855abe28bcfb709840206143d80
Moe Thet Khine <soniapoudel03@gmail.com>
2024-11-23 08:36:34 +0630

    Format Code

--- MTKDotNetCore.MiniKpay.Database/Models/BaseResponseModel.cs
+++ MTKDotNetCore.MiniKpay.Database/Models/BaseResponseModel.cs
@@ -1,69 +1,68 @@
-﻿namespace MTKDotNetCore.MiniKpay.Database.Models
+﻿namespace MTKDotNetCore.MiniKpay.Database.Models;
+
+public class BaseResponseModel
 {
-    public class BaseResponseModel
+    public string RespCode { get; set; }
+    public string RespDesp {  get; set; }
+    public EnumRespType RespType { get; set; }
+    public bool IsSuccess { get; set; }
+    public bool IsError { get { return !IsSuccess; } }
+
+    #region Success
+
+    public static BaseResponseModel Success(string respCode,string respDesp)
     {
-        public string RespCode { get; set; }
-        public string RespDesp {  get; set; }
-        public EnumRespType RespType { get; set; }
-        public bool IsSuccess { get; set; }
-        public bool IsError { get { return !IsSuccess; } }
-
-        #region Success
-
-        public static BaseResponseModel Success(string respCode,string respDesp)
+        return new BaseResponseModel
         {
-            return new BaseResponseModel
-            {
-                IsSuccess = true,
-                RespCode = respCode,
-                RespDesp = respDesp,
-                RespType = EnumRespType.Success,
-            };
-        }
-
-        #endregion
-
-        #region ValidationError
-
-        public static BaseResponseModel ValidationError(string respCode, string respDesp)
-        {
-            return new BaseResponseModel
-            {
-                IsSuccess = false,
-                RespCode = respCode,
-                RespDesp = respDesp,
-                RespType = EnumRespType.ValidationError,
-            };
-        }
-
-        #endregion
-
-        #region SystemError
-
-        public static BaseResponseModel SystemError(string respCode, string respDesp)
-        {
-            return new BaseResponseModel
-            {
-                IsSuccess = false,
-                RespCode = respCode,
-                RespDesp = respDesp,
-                RespType = EnumRespType.ValidationError,
-            };
-        }
-
-        #endregion
-
-    }
-
-    #region EnumRespType
-
-    public enum EnumRespType
-    {
-        None,
-        Success,
-        ValidationError,
-        SystemError
+            IsSuccess = true,
+            RespCode = respCode,
+            RespDesp = respDesp,
+            RespType = EnumRespType.Success,
+        };
     }
 
     #endregion
+
+    #region ValidationError
+
+    public static BaseResponseModel ValidationError(string respCode, string respDesp)
+    {
+        return new BaseResponseModel
+        {
+            IsSuccess = false,
+            RespCode = respCode,
+            RespDesp = respDesp,
+            RespType = EnumRespType.ValidationError,
+        };
+    }
+
+    #endregion
+
+    #region SystemError
+
+    public static BaseResponseModel SystemError(string respCode, string respDesp)
+    {
+        return new BaseResponseModel
+        {
+            IsSuccess = false,
+            RespCode = respCode,
+            RespDesp = respDesp,
+            RespType = EnumRespType.ValidationError,
+        };
+    }
+
+    #endregion
+
 }
+
+#region EnumRespType
+
+public enum EnumRespType
+{
+    None,
+    Success,
+    ValidationError,
+    SystemError
+}
+
+#endregion
