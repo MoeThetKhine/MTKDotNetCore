8ad48961c4381f9bdbe8cc607cfe2547e2567a4c
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-01 21:15:46 +0630

    Feat : Add BlogModel

---
+++ MTKDotNetCore.ConsoleApp3/IBlogApi.cs
@@ -0,0 +1,23 @@
+﻿using Refit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MTKDotNetCore.ConsoleApp3
+{
+    public interface IBlogApi
+    {
+       
+    }
+
+    public class BlogModel
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+        public bool DeleteFlag {  get; set; }
+    }
+}
