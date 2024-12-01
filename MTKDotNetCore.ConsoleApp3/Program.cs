6cd4ba82fcdab6b18c0c291ed78e8a6abe7ff0c7
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-01 11:53:11 +0630

    Feat : Add in Program.cs

--- MTKDotNetCore.ConsoleApp3/Program.cs
+++ MTKDotNetCore.ConsoleApp3/Program.cs
@@ -6,3 +6,10 @@
 //await httpClientExample.Edit(78);
 //await httpClientExample.Create("sss", "sss", 1);
 //await httpClientExample.Update(1, "ddd", "ddd", 1 );
+
+//RestClientExample restClientExample = new RestClientExample();
+//await restClientExample.Read();
+//await restClientExample.Edit(2);
+//await restClientExample.Delete(3);
+//await restClientExample.Create("sss", "sss", 1);
+//await restClientExample.Update(1, "ddd", "ddd", 1);
