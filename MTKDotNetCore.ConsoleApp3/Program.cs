044f9b616b1b0a8899fdcc81c02663a22c58aff0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-01 11:44:58 +0630

    Feat: Add PostModel in RestClient

--- MTKDotNetCore.ConsoleApp3/Program.cs
+++ MTKDotNetCore.ConsoleApp3/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿using MTKDotNetCore.ConsoleApp3;
+
+//HttpClientExample httpClientExample = new HttpClientExample();
+//await httpClientExample.Read();
+//await httpClientExample.Edit(2);
+//await httpClientExample.Edit(78);
+//await httpClientExample.Create("sss", "sss", 1);
+//await httpClientExample.Update(1, "ddd", "ddd", 1 );
---
+++ MTKDotNetCore.ConsoleApp3/RestClientExample.cs
@@ -0,0 +1,30 @@
+﻿using RestSharp;
+
+namespace MTKDotNetCore.ConsoleApp3
+{
+    public class RestClientExample
+    {
+        private readonly RestClient _client;
+        private readonly string _postEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
+        public RestClientExample(RestClient client)
+        {
+            _client = client;
+        }
+
+       
+
+    }
+
+    #region PostModel
+
+    public class PostModel
+    {
+        public int userId { get; set; }
+        public int id { get; set; }
+        public string title { get; set; }
+        public string body { get; set; }
+    }
+
+    #endregion
+}
