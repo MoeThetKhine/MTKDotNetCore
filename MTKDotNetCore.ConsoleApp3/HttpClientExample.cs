299b6c32139962a1c56fd113b951b6602deac674
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-04 10:07:42 +0630

    Feat : Add PostModel

--- MTKDotNetCore.ConsoleApp3/HttpClientExample.cs
+++ MTKDotNetCore.ConsoleApp3/HttpClientExample.cs
@@ -2,5 +2,25 @@
 {
     public class HttpClientExample
     {
+        private readonly HttpClient _client;
+        private readonly string _postEndpoint = "https://jsonplaceholder.typicode.com/posts";
+
+        public HttpClientExample(HttpClient client)
+        {
+            _client = client;
+        }
+
+        #region PostModel
+
+        public class PostModel
+        {
+            public int userId { get; set; }
+            public int id { get; set; }
+            public string title { get; set; }
+            public string body { get; set; }
+
+        }
+
+        #endregion
     }
 }
