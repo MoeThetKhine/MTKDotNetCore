58bea09eda5145ee0c905a3fb855e4d444374736
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-04 11:13:02 +0630

    Feat : Add Global Using

--- MTKDotNetCore.ConsoleApp3/Program.cs
+++ MTKDotNetCore.ConsoleApp3/Program.cs
@@ -1,6 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-using MTKDotNetCore.ConsoleApp3;
-
+﻿
 Console.WriteLine("Hello, World!");
 
 HttpClientExample client = new HttpClientExample();
