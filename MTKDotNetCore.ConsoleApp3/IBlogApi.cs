8a186357f31ffeb72de36d83fd9e1c10546621e0
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-04 13:02:14 +0630

    Format Code

--- MTKDotNetCore.ConsoleApp3/IBlogApi.cs
+++ MTKDotNetCore.ConsoleApp3/IBlogApi.cs
@@ -1,16 +1,15 @@
 ﻿using Refit;
 
-namespace MTKDotNetCore.ConsoleApp3
+namespace MTKDotNetCore.ConsoleApp3;
+
+public interface IBlogApi
 {
-    public interface IBlogApi
-    {
-        [Get("/api/blogs")]
-        Task<List<BlogModel>> GetBlogs();
+    [Get("/api/blogs")]
+    Task<List<BlogModel>> GetBlogs();
 
-        [Get("/api/blogs/{id}")]
-        Task<List<BlogModel>> GetBlog();
+    [Get("/api/blogs/{id}")]
+    Task<List<BlogModel>> GetBlog();
 
-        [Get("/api/blogs")]
-        Task<BlogModel> CreateBlog(BlogModel blog);
-    }
+    [Get("/api/blogs")]
+    Task<BlogModel> CreateBlog(BlogModel blog);
 }
