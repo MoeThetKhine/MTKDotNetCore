13142d04359588a54379684a9097aa04ac0e0a5c
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-04 16:52:02 +0630

    Feat : Update AppDbContext

--- MTKDotNetCore.Database/Models/AppDbContext.cs
+++ MTKDotNetCore.Database/Models/AppDbContext.cs
@@ -2,25 +2,20 @@
 
 public partial class AppDbContext : DbContext
 {
-    public AppDbContext()
-    {
-    }
-
-    public AppDbContext(DbContextOptions<AppDbContext> options)
-        : base(options)
+    public AppDbContext(DbContextOptions options): base(options)
     {
     }
 
     public virtual DbSet<TblBlog> TblBlogs { get; set; }
 
-    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-    {
-        if (!optionsBuilder.IsConfigured)
-        {
-            string connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
-            optionsBuilder.UseSqlServer(connectionString);
-        }
-    }
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    if (!optionsBuilder.IsConfigured)
+    //    {
+    //        string connectionString = "Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;TrustServerCertificate=True;";
+    //        optionsBuilder.UseSqlServer(connectionString);
+    //    }
+    //}
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
