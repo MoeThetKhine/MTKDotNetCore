33de46fe99366a5eabe373572867a914be5bf41a
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-04 11:22:51 +0630

    -

--- MTKDotNetCore.ConsoleApp3/Program.cs
+++ MTKDotNetCore.ConsoleApp3/Program.cs
@@ -1,10 +1,10 @@
 ﻿Console.WriteLine("Hello, World!");
 
-HttpClientExample client = new HttpClientExample();
+//HttpClientExample client = new HttpClientExample();
 //await client.ReadAsync();
 //await client.EditAsync(2);
 //await client.EditAsync(200);
 //await client.CreateAsync("test title", "test body", 3);
 //await client.UpdateAsync(1, "ddd", "ddd", 1);
-await client.DeleteAsync(2);
+//await client.DeleteAsync(2);
 
