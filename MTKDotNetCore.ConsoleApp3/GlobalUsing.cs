8525de35b998635920c6d57cac18864013f0a517
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-04 13:03:02 +0630

    Feat : Add Global Using

--- MTKDotNetCore.ConsoleApp3/GlobalUsing.cs
+++ MTKDotNetCore.ConsoleApp3/GlobalUsing.cs
@@ -3,3 +3,4 @@ global using Newtonsoft.Json;
 global using System.Text;
 global using static System.Net.Mime.MediaTypeNames;
 global using RestSharp;
+global using Refit;
--- MTKDotNetCore.ConsoleApp3/IBlogApi.cs
+++ MTKDotNetCore.ConsoleApp3/IBlogApi.cs
@@ -1,5 +1,4 @@
-﻿using Refit;
-
+﻿
 namespace MTKDotNetCore.ConsoleApp3;
 
 public interface IBlogApi
