65928f14d545c28bee83a309c37bb57c9686e66c
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-04 12:58:18 +0630

    Format Code

--- MTKDotNetCore.ConsoleApp3/BlogModel.cs
+++ MTKDotNetCore.ConsoleApp3/BlogModel.cs
@@ -1,11 +1,10 @@
-﻿namespace MTKDotNetCore.ConsoleApp3
+﻿namespace MTKDotNetCore.ConsoleApp3;
+
+public class BlogModel
 {
-    public class BlogModel
-    {
-        public int BlogId { get; set; }
-        public string BlogTitle { get; set; }
-        public string BlogAuthor { get; set; }
-        public string BlogContent { get; set; }
-        public bool DeleteFlag { get; set; }
-    }
+    public int BlogId { get; set; }
+    public string BlogTitle { get; set; }
+    public string BlogAuthor { get; set; }
+    public string BlogContent { get; set; }
+    public bool DeleteFlag { get; set; }
 }
