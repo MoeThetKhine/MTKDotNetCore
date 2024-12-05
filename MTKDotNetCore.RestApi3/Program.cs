6bb914e12de0ab968da3e69913b39a6fa6211344
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-05 16:21:29 +0630

    Feat : Add Global Using

--- MTKDotNetCore.RestApi3/Program.cs
+++ MTKDotNetCore.RestApi3/Program.cs
@@ -1,22 +1,32 @@
-using Refit;
-using RestSharp;
-
 var builder = WebApplication.CreateBuilder(args);
-
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+#region Dependency Injection for HttpClient
+
 builder.Services.AddSingleton(n => new HttpClient()
 {
     BaseAddress = new Uri(builder.Configuration.GetSection("ApiDomainUrl").Value!)
 });
+
+#endregion
+
+#region Dependency Injection for RestClient
+
 builder.Services.AddSingleton(n =>
     new RestClient(builder.Configuration.GetSection("ApiDomainUrl").Value!));
+
+#endregion
+
+#region Dependency Injection for RefitClient
+
 builder.Services
     .AddRefitClient<ISnakeApi>()
     .ConfigureHttpClient(c => c.BaseAddress = new Uri(builder.Configuration.GetSection("ApiDomainUrl").Value!));
+
+#endregion
 
 
 var app = builder.Build();
@@ -35,12 +45,17 @@ app.MapControllers();
 
 app.Run();
 
+#region ISnakeApi
+
 public interface ISnakeApi
 {
     [Get("/snakes")]
     Task<List<SnakeModel>> GetSnakes();
 }
 
+#endregion
+
+#region SnakeModel
 
 public class SnakeModel
 {
@@ -52,3 +67,5 @@ public class SnakeModel
     public string IsPoison { get; set; }
     public string IsDanger { get; set; }
 }
+
+#endregion
