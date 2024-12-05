ae487db3dcce590eaf1f0b40a0e2333091ce04dd
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-05 23:47:54 +0630

    Feat : Add Read

--- MTKDotNetCore.ConsoleApp4/AdoDotNet/AdoDotNetExample.cs
+++ MTKDotNetCore.ConsoleApp4/AdoDotNet/AdoDotNetExample.cs
@@ -1,6 +1,37 @@
-﻿namespace MTKDotNetCore.ConsoleApp4.AdoDotNet
+﻿using MTKDotNetCore.Shared;
+using System.Data;
+
+namespace MTKDotNetCore.ConsoleApp4.AdoDotNet
 {
     public class AdoDotNetExample
     {
+        private readonly AdoDotNetService _adoDotNetService;
+
+        public AdoDotNetExample(AdoDotNetService adoDotNetService)
+        {
+            _adoDotNetService = adoDotNetService;
+        }
+
+        #region Read
+
+        public void Read()
+        {
+            string query = @"SELECT [BlogId]
+      ,[BlogTitle]
+      ,[BlogAuthor]
+      ,[BlogContent]
+      ,[DeleteFlag]
+  FROM [dbo].[Tbl_Blog] where DeleteFlag = 0";
+            var dt = _adoDotNetService.Query(query);
+            foreach (DataRow dr in dt.Rows)
+            {
+                Console.WriteLine(dr["BlogId"]);
+                Console.WriteLine(dr["BlogTitle"]);
+                Console.WriteLine(dr["BlogAuthor"]);
+                Console.WriteLine(dr["BlogContent"]);
+            }
+        }
+
+        #endregion
     }
 }
