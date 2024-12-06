326bb1092c2cb52be306e1a4281cd1c21e58311f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-06 10:51:23 +0630

    Complete Adding DI in AdoDotNetExample

--- MTKDotNetCore.ConsoleApp4/Program.cs
+++ MTKDotNetCore.ConsoleApp4/Program.cs
@@ -1,1 +1,27 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Microsoft.Extensions.DependencyInjection;
+using MTKDotNetCore.ConsoleApp4.AdoDotNet;
+
+Console.WriteLine("Hello, World!");
+
+var services = new ServiceCollection()
+            .AddSingleton<AdoDotNetService>(provider =>
+            new AdoDotNetService("Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;"))
+            .AddSingleton<AdoDotNetExample>()
+            .BuildServiceProvider();
+
+var adoDotNetExample = services.GetRequiredService<AdoDotNetExample>();
+
+Console.WriteLine("Read");
+adoDotNetExample.Read();
+
+Console.WriteLine("Create");
+adoDotNetExample.Create();
+
+Console.WriteLine(" Edit");
+adoDotNetExample.Edit();
+
+Console.WriteLine("Delete");
+adoDotNetExample.Delete();
+
+Console.WriteLine("Update");
+adoDotNetExample.Update();
