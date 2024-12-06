4180462bee3688cca716ae8755f7ceee505e9aae
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-06 15:46:26 +0630

    Complete Adding Dependency Injection in Dapper

--- MTKDotNetCore.ConsoleApp4/GlobalUsing.cs
+++ MTKDotNetCore.ConsoleApp4/GlobalUsing.cs
@@ -4,3 +4,4 @@ global using System.Data;
 global using static MTKDotNetCore.ConsoleApp4.AdoDotNet.AdoDotNetService;
 global using System.Data.SqlClient;
 global using Dapper;
+global using MTKDotNetCore.ConsoleApp4.Dapper;
--- MTKDotNetCore.ConsoleApp4/Program.cs
+++ MTKDotNetCore.ConsoleApp4/Program.cs
@@ -1,50 +1,53 @@
-﻿#region DI In AdoDotNetExample
+﻿
+#region DI In AdoDotNetExample
 
 #region DI
 
-using MTKDotNetCore.ConsoleApp4.Dapper;
+//var services = new ServiceCollection()
+//            .AddSingleton<AdoDotNetService>(provider =>
+//            new AdoDotNetService("Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;"))
+//            .AddSingleton<AdoDotNetExample>()
+//            .BuildServiceProvider();
 
-var services = new ServiceCollection()
-            .AddSingleton<AdoDotNetService>(provider =>
-            new AdoDotNetService("Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;"))
-            .AddSingleton<AdoDotNetExample>()
-            .BuildServiceProvider();
-
-var adoDotNetExample = services.GetRequiredService<AdoDotNetExample>();
+//var adoDotNetExample = services.GetRequiredService<AdoDotNetExample>();
 
 #endregion
 
-Console.WriteLine("Read");
-adoDotNetExample.Read();
+//Console.WriteLine("Read");
+//adoDotNetExample.Read();
 
-Console.WriteLine("Create");
-adoDotNetExample.Create();
+//Console.WriteLine("Create");
+//adoDotNetExample.Create();
 
-Console.WriteLine(" Edit");
-adoDotNetExample.Edit();
+//Console.WriteLine(" Edit");
+//adoDotNetExample.Edit();
 
-Console.WriteLine("Delete");
-adoDotNetExample.Delete();
+//Console.WriteLine("Delete");
+//adoDotNetExample.Delete();
 
-Console.WriteLine("Update");
-adoDotNetExample.Update();
+//Console.WriteLine("Update");
+//adoDotNetExample.Update();
 
 #endregion
 
 #region DI In Dapper
 
-var service1 = new ServiceCollection()
-           .AddSingleton<DapperService>(provider => new DapperService("Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;"))
-           .AddSingleton<DapperExample>()
-           .AddSingleton<DapperQueries>()
-           .BuildServiceProvider();
+#region DI
 
-var dapperExample = services.GetRequiredService<DapperExample>();
+//var service1 = new ServiceCollection()
+//           .AddSingleton<DapperService>(provider => new DapperService("Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;"))
+//           .AddSingleton<DapperExample>()
+//           .AddSingleton<DapperQueries>()
+//           .BuildServiceProvider();
 
-dapperExample.Read();
-dapperExample.Create("New Blog", "Author", "Content");
-dapperExample.Edit(1);
-dapperExample.Delete(1);
-dapperExample.Update(1, "Updated Blog", "Updated Author", "Updated Content");
+#endregion
+
+//var dapperExample = service1.GetRequiredService<DapperExample>();
+
+//dapperExample.Read();
+//dapperExample.Create("New Blog", "Author", "Content");
+//dapperExample.Edit(1);
+//dapperExample.Update(1, "Updated Blog", "Updated Author", "Updated Content");
+//dapperExample.Delete(78);
 
 #endregion
