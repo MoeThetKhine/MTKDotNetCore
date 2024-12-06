258b2bc7a1d913363b0a95445b7eb67700b9d456
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-06 14:48:23 +0630

    Feat : Add Read

--- MTKDotNetCore.ConsoleApp4/Dapper/DapperExample.cs
+++ MTKDotNetCore.ConsoleApp4/Dapper/DapperExample.cs
@@ -3,15 +3,28 @@
     public class DapperExample
     {
         private readonly DapperService _dapperService;
+        private readonly DapperQueries _dapperQueries;
 
-        public DapperExample(DapperService dapperService)
+        public DapperExample(DapperService dapperService, DapperQueries dapperQueries)
         {
             _dapperService = dapperService;
+            _dapperQueries = dapperQueries;
         }
+
+        #region Read
 
         public void Read()
         {
-            string query = 
+            var lst = _dapperService.Query<BlogDataDapperModel>(_dapperQueries.GetReadQuery()).ToList();
+            foreach (var item in lst)
+            {
+                Console.WriteLine(item.BlogId);
+                Console.WriteLine(item.BlogTitle);
+                Console.WriteLine(item.BlogAuthor);
+                Console.WriteLine(item.BlogContent);
+            }
         }
+
+        #endregion
     }
 }
