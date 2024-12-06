9d6fa6eb99e184ca92598d100359626f36bdb5f9
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-06 14:41:32 +0630

    Format Code

--- MTKDotNetCore.ConsoleApp4/Dapper/DapperQueries.cs
+++ MTKDotNetCore.ConsoleApp4/Dapper/DapperQueries.cs
@@ -1,26 +1,25 @@
-﻿namespace MTKDotNetCore.ConsoleApp4.Dapper
-{
-    public class DapperQueries
-    {
-        public string GetReadQuery() =>
-            "SELECT * FROM tbl_blog WHERE DeleteFlag = 0;";
+﻿namespace MTKDotNetCore.ConsoleApp4.Dapper;
 
-        public string GetCreateQuery() => @"INSERT INTO [dbo].[Tbl_Blog]
+public class DapperQueries
+{
+    public string GetReadQuery() =>
+        "SELECT * FROM tbl_blog WHERE DeleteFlag = 0;";
+
+    public string GetCreateQuery() => @"INSERT INTO [dbo].[Tbl_Blog]
            ([BlogTitle], [BlogAuthor], [BlogContent], [DeleteFlag])
            VALUES (@BlogTitle, @BlogAuthor, @BlogContent, 0);";
 
-        public string GetEditQuery() => 
-            "SELECT * FROM tbl_blog WHERE DeleteFlag = 0 AND BlogId = @BlogId;";
+    public string GetEditQuery() => 
+        "SELECT * FROM tbl_blog WHERE DeleteFlag = 0 AND BlogId = @BlogId;";
 
-        public string GetDeleteQuery() => @"UPDATE [dbo].[Tbl_Blog]
+    public string GetDeleteQuery() => @"UPDATE [dbo].[Tbl_Blog]
                          SET [DeleteFlag] = 1
                          WHERE BlogId = @BlogId;";
 
-        public string GetUpdateQuery() => @"UPDATE [dbo].[Tbl_Blog]
+    public string GetUpdateQuery() => @"UPDATE [dbo].[Tbl_Blog]
                          SET [BlogTitle] = @BlogTitle,
                              [BlogAuthor] = @BlogAuthor,
                              [BlogContent] = @BlogContent,
                              [DeleteFlag] = 0
                          WHERE BlogId = @BlogId;";
-    }
 }
