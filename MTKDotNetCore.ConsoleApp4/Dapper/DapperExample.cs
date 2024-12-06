3f2e8ad3ee457b8e8b5fda4c730c9ff05dec797f
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-06 14:41:17 +0630

    Feat : Add DapperQueries

--- MTKDotNetCore.ConsoleApp4/Dapper/DapperExample.cs
+++ MTKDotNetCore.ConsoleApp4/Dapper/DapperExample.cs
@@ -2,6 +2,16 @@
 {
     public class DapperExample
     {
+        private readonly DapperService _dapperService;
 
+        public DapperExample(DapperService dapperService)
+        {
+            _dapperService = dapperService;
+        }
+
+        public void Read()
+        {
+            string query = 
+        }
     }
 }
