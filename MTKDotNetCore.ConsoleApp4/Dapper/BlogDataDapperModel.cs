101fbf07138ca5a30478fccae702a11fb92ffc27
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-06 14:06:55 +0630

    Feat : Add BlogDataDapperModel

---
+++ MTKDotNetCore.ConsoleApp4/Dapper/BlogDataDapperModel.cs
@@ -0,0 +1,16 @@
+﻿namespace MTKDotNetCore.ConsoleApp4.Dapper
+{
+    #region BlogDataDapperModel
+
+    public class BlogDataDapperModel
+    {
+        public int BlogId { get; set; }
+        public string BlogTitle { get; set; }
+        public string BlogAuthor { get; set; }
+        public string BlogContent { get; set; }
+        public int DeleteFlag { get; set; }
+    }
+
+    #endregion
+
+}
