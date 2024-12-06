a46586c157cfc89f8cb6a12ef6e5f959807ba755
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-06 20:39:46 +0630

    Format Code

--- MTKDotNetCore.ConsoleApp4/EFCore/BlogModel.cs
+++ MTKDotNetCore.ConsoleApp4/EFCore/BlogModel.cs
@@ -1,18 +1,15 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿namespace MTKDotNetCore.ConsoleApp4.EFCore;
 
-namespace MTKDotNetCore.ConsoleApp4.EFCore
+public class BlogModel
 {
-    public class BlogModel
-    {
-        [Key]
-        public long BlogId { get; set; }
+    [Key]
+    public long BlogId { get; set; }
 
-        public string BlogTitle { get; set; } = null!;
+    public string BlogTitle { get; set; } = null!;
 
-        public string BlogAuthor { get; set; } = null!;
+    public string BlogAuthor { get; set; } = null!;
 
-        public string BlogContent { get; set; } = null!;
+    public string BlogContent { get; set; } = null!;
 
-        public bool DeleteFlag { get; set; }
-    }
+    public bool DeleteFlag { get; set; }
 }
