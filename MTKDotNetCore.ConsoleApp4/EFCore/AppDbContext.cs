7ecff7d829f837388099a0407667944811412f8e
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-06 20:19:11 +0630

    Feat : Add AppDbContext

---
+++ MTKDotNetCore.ConsoleApp4/EFCore/AppDbContext.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MTKDotNetCore.Database.Models;
+
+namespace MTKDotNetCore.ConsoleApp4.EFCore
+{
+    public partial class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public virtual DbSet<TblBlog> TblBlogs { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<TblBlog>(entity =>
+            {
+                entity.HasKey(e => e.BlogId);
+
+                entity.ToTable("Tbl_Blog");
+
+                entity.Property(e => e.BlogAuthor).HasMaxLength(50);
+                entity.Property(e => e.BlogTitle).HasMaxLength(50);
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
