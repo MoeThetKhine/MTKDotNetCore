fc0226da4b02ee0657cc713fc371197aa98e098c
Moe Thet Khine <soniapoudel03@gmail.com>
2024-12-06 15:42:56 +0630

    Feat : Add DI in Dapper

--- MTKDotNetCore.ConsoleApp4/Program.cs
+++ MTKDotNetCore.ConsoleApp4/Program.cs
@@ -2,6 +2,8 @@
 
 #region DI
 
+using MTKDotNetCore.ConsoleApp4.Dapper;
+
 var services = new ServiceCollection()
             .AddSingleton<AdoDotNetService>(provider =>
             new AdoDotNetService("Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;"))
@@ -26,5 +28,23 @@ adoDotNetExample.Delete();
 
 Console.WriteLine("Update");
 adoDotNetExample.Update();
+
+#endregion
+
+#region DI In Dapper
+
+var service1 = new ServiceCollection()
+           .AddSingleton<DapperService>(provider => new DapperService("Data Source=.;Initial Catalog=DotNetTrainingBatch5;User ID=sa;Password=sasa@123;"))
+           .AddSingleton<DapperExample>()
+           .AddSingleton<DapperQueries>()
+           .BuildServiceProvider();
+
+var dapperExample = services.GetRequiredService<DapperExample>();
+
+dapperExample.Read();
+dapperExample.Create("New Blog", "Author", "Content");
+dapperExample.Edit(1);
+dapperExample.Delete(1);
+dapperExample.Update(1, "Updated Blog", "Updated Author", "Updated Content");
 
 #endregion
