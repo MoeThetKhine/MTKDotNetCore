7783b3ed948ee5ba5825b0e19fd0b48ee11f4d16
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-04 01:58:42 +0630

    Feat : Add BlogRequestModel

--- MTKDotNetCore.MvcApp/Models/BlogRequestModel.cs
+++ MTKDotNetCore.MvcApp/Models/BlogRequestModel.cs
@@ -2,5 +2,9 @@
 {
     public class BlogRequestModel
     {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Content { get; set; } 
     }
 }
