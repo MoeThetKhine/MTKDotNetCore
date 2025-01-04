e395bdf6f2824752e2f81b8fa2abe3c27b3e04fe
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-04 20:05:38 +0630

    Format Code

--- MTKDotNetCore.MvcApp/Controllers/HomeController.cs
+++ MTKDotNetCore.MvcApp/Controllers/HomeController.cs
@@ -2,60 +2,59 @@
 using MTKDotNetCore.MvcApp.Models;
 using System.Diagnostics;
 
-namespace MTKDotNetCore.MvcApp.Controllers
+namespace MTKDotNetCore.MvcApp.Controllers;
+
+public class HomeController : Controller
 {
-    public class HomeController : Controller
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
     {
-        private readonly ILogger<HomeController> _logger;
-
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
-
-        #region Index
-
-        public IActionResult Index()
-        {
-            ViewBag.Message = "Hello from Viewbag";
-            ViewData["Message2"] = "Hello from ViewData";
-
-            HomeResponseModel model = new HomeResponseModel();
-            model.AlertMessage = "Hello from Model";
-
-            //return Redirect("/Home/Privacy");
-
-            return View(model);
-        }
-
-        #endregion
-
-        #region Privacy
-
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        #endregion
-
-        #region Error
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        #endregion
-
-        #region Index2
-
-        public IActionResult Index2()
-        {
-            return View();
-        }
-
-        #endregion
+        _logger = logger;
     }
+
+    #region Index
+
+    public IActionResult Index()
+    {
+        ViewBag.Message = "Hello from Viewbag";
+        ViewData["Message2"] = "Hello from ViewData";
+
+        HomeResponseModel model = new HomeResponseModel();
+        model.AlertMessage = "Hello from Model";
+
+        //return Redirect("/Home/Privacy");
+
+        return View(model);
+    }
+
+    #endregion
+
+    #region Privacy
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    #endregion
+
+    #region Error
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    #endregion
+
+    #region Index2
+
+    public IActionResult Index2()
+    {
+        return View();
+    }
+
+    #endregion
 }
