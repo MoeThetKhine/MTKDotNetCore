1e3af613592a00a182eebf5e23f1664e9b8c8111
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-04 21:46:21 +0630

    Feat : Add BlogCreate

--- MTKDotNetCore.MvcApp/Controllers/BlogController.cs
+++ MTKDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -4,9 +4,26 @@ namespace MTKDotNetCore.MvcApp.Controllers
 {
     public class BlogController : Controller
     {
+        private readonly IBlogService _blogService;
+
+        public BlogController(IBlogService blogService)
+        {
+            _blogService = blogService;
+        }
+
         public IActionResult Index()
         {
+            var lst = _blogService.GetBlogs();
             return View();
         }
+
+        [ActionName("Create")]
+        public IActionResult BlogCreate()
+        {
+            return View("BlogCreate");
+        }
+
+
+
     }
 }
