8b9f069b568c4163353a87dad846a7b87a49901c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-04 21:55:10 +0630

    Format Code

--- MTKDotNetCore.MvcApp/Controllers/BlogController.cs
+++ MTKDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -1,104 +1,103 @@
-﻿namespace MTKDotNetCore.MvcApp.Controllers
+﻿namespace MTKDotNetCore.MvcApp.Controllers;
+
+public class BlogController : Controller
 {
-    public class BlogController : Controller
+    private readonly IBlogService _blogService;
+
+    public BlogController(IBlogService blogService)
     {
-        private readonly IBlogService _blogService;
+        _blogService = blogService;
+    }
 
-        public BlogController(IBlogService blogService)
-        {
-            _blogService = blogService;
-        }
+    public IActionResult Index()
+    {
+        var lst = _blogService.GetBlogs();
+        return View();
+    }
 
-        public IActionResult Index()
+    [ActionName("Create")]
+    public IActionResult BlogCreate()
+    {
+        return View("BlogCreate");
+    }
+    [HttpPost]
+    [ActionName("Save")]
+    public IActionResult BlogSave(BlogRequestModel requestModel)
+    {
+        try
         {
-            var lst = _blogService.GetBlogs();
-            return View();
-        }
-
-        [ActionName("Create")]
-        public IActionResult BlogCreate()
-        {
-            return View("BlogCreate");
-        }
-        [HttpPost]
-        [ActionName("Save")]
-        public IActionResult BlogSave(BlogRequestModel requestModel)
-        {
-            try
+            _blogService.CreateBlog(new TblBlog
             {
-                _blogService.CreateBlog(new TblBlog
-                {
-                    BlogAuthor = requestModel.Author,
-                    BlogContent = requestModel.Content,
-                    BlogTitle = requestModel.Title
-                });
+                BlogAuthor = requestModel.Author,
+                BlogContent = requestModel.Content,
+                BlogTitle = requestModel.Title
+            });
 
-                //ViewBag.IsSuccess = true;
-                //ViewBag.Message = "Blog Created Successfully";
+            //ViewBag.IsSuccess = true;
+            //ViewBag.Message = "Blog Created Successfully";
 
-                TempData["IsSuccess"] = true;
-                TempData["Message"] = "Blog Created Successfully";
-            }
-            catch (Exception ex)
-            {
-                //ViewBag.IsSuccess = false;
-                //ViewBag.Message = ex.ToString();
-
-                TempData["IsSuccess"] = false;
-                TempData["Message"] = ex.ToString();
-            }
-
-            return RedirectToAction("Index");
-
-            //var lst = _blogService.GetBlogs();
-            //return View("Index", lst);
+            TempData["IsSuccess"] = true;
+            TempData["Message"] = "Blog Created Successfully";
         }
-
-        [ActionName("Delete")]
-        public IActionResult BlogDelete(int id)
+        catch (Exception ex)
         {
-            _blogService.DelteBlog(id);
-            return RedirectToAction("Index");
+            //ViewBag.IsSuccess = false;
+            //ViewBag.Message = ex.ToString();
+
+            TempData["IsSuccess"] = false;
+            TempData["Message"] = ex.ToString();
         }
 
-        [ActionName("Edit")]
-        public IActionResult BlogEdit(int id)
+        return RedirectToAction("Index");
+
+        //var lst = _blogService.GetBlogs();
+        //return View("Index", lst);
+    }
+
+    [ActionName("Delete")]
+    public IActionResult BlogDelete(int id)
+    {
+        _blogService.DelteBlog(id);
+        return RedirectToAction("Index");
+    }
+
+    [ActionName("Edit")]
+    public IActionResult BlogEdit(int id)
+    {
+        var blog = _blogService.GetBlog(id);
+        BlogRequestModel blogRequestModel = new BlogRequestModel
         {
-            var blog = _blogService.GetBlog(id);
-            BlogRequestModel blogRequestModel = new BlogRequestModel
-            {
-                Id = blog.BlogId,
-                Author = blog.BlogAuthor,
-                Content = blog.BlogContent,
-                Title = blog.BlogTitle
-            };
-            return View("BlogEdit", blogRequestModel);
-        }
+            Id = blog.BlogId,
+            Author = blog.BlogAuthor,
+            Content = blog.BlogContent,
+            Title = blog.BlogTitle
+        };
+        return View("BlogEdit", blogRequestModel);
+    }
 
-        [HttpPost]
-        [ActionName("Update")]
-        public IActionResult BlogUpdate(int id, BlogRequestModel requestModel)
+    [HttpPost]
+    [ActionName("Update")]
+    public IActionResult BlogUpdate(int id, BlogRequestModel requestModel)
+    {
+        try
         {
-            try
+            _blogService.UpdateBlog(id, new TblBlog
             {
-                _blogService.UpdateBlog(id, new TblBlog
-                {
-                    BlogAuthor = requestModel.Author,
-                    BlogContent = requestModel.Content,
-                    BlogTitle = requestModel.Title
-                });
+                BlogAuthor = requestModel.Author,
+                BlogContent = requestModel.Content,
+                BlogTitle = requestModel.Title
+            });
 
-                TempData["IsSuccess"] = true;
-                TempData["Message"] = "Blog Updated Successfully";
-            }
-            catch (Exception ex)
-            {
-                TempData["IsSuccess"] = false;
-                TempData["Message"] = ex.ToString();
-            }
-
-            return RedirectToAction("Index");
+            TempData["IsSuccess"] = true;
+            TempData["Message"] = "Blog Updated Successfully";
         }
+        catch (Exception ex)
+        {
+            TempData["IsSuccess"] = false;
+            TempData["Message"] = ex.ToString();
+        }
+
+        return RedirectToAction("Index");
     }
 }
 
