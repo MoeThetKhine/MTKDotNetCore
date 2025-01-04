9814c2894a7d8cc6d057c3b24d67a60b7235d849
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-04 19:57:23 +0630

    Feat : Add Index2

--- MTKDotNetCore.MvcApp/Controllers/HomeController.cs
+++ MTKDotNetCore.MvcApp/Controllers/HomeController.cs
@@ -37,6 +37,9 @@ namespace MTKDotNetCore.MvcApp.Controllers
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
 
-        
+        public IActionResult Index2()
+        {
+            return View();
+        }
     }
 }
