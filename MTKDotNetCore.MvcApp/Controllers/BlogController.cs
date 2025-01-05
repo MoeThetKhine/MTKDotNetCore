4acc6a01898809f90c02ea09343166b4db5c613e
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-05 10:42:02 +0630

    Feat : Add Blog Save

--- MTKDotNetCore.MvcApp/Controllers/BlogController.cs
+++ MTKDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -24,5 +24,42 @@ namespace MTKDotNetCore.MvcApp.Controllers
             return View("BlogCreate");
         }
 
+
+
+        [HttpPost]
+        [ActionName("Save")]
+        public IActionResult BlogSave(BlogRequestModel requestModel)
+        {
+            try
+            {
+                _blogService.CreateBlog(new TblBlog
+                {
+                    BlogAuthor = requestModel.Author,
+                    BlogContent = requestModel.Content,
+                    BlogTitle = requestModel.Title
+                });
+
+                //ViewBag.IsSuccess = true;
+                //ViewBag.Message = "Blog Created Successfully";
+
+                TempData["IsSuccess"] = true;
+                TempData["Message"] = "Blog Created Successfully";
+            }
+            catch (Exception ex)
+            {
+                //ViewBag.IsSuccess = false;
+                //ViewBag.Message = ex.ToString();
+
+                TempData["IsSuccess"] = false;
+                TempData["Message"] = ex.ToString();
+            }
+
+            return RedirectToAction("Index");
+
+            //var lst = _blogService.GetBlogs();
+            //return View("Index", lst);
+        }
+
+
     }
 }
