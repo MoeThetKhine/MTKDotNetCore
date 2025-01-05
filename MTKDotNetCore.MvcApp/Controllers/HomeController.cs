b8005a15df930fcd2ae64428cbab2d84ea474020
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-05 10:37:30 +0630

    Feat : Add Index

--- MTKDotNetCore.MvcApp/Controllers/HomeController.cs
+++ MTKDotNetCore.MvcApp/Controllers/HomeController.cs
@@ -13,20 +13,19 @@ namespace MTKDotNetCore.MvcApp.Controllers
             _logger = logger;
         }
 
-        public IActionResult Index()
-        {
-            return View();
-        }
 
-        public IActionResult Privacy()
-        {
-            return View();
-        }
 
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-    }
+		public IActionResult Index()
+		{
+			ViewBag.Message = "Hello from Viewbag";
+			ViewData["Message2"] = "Hello from ViewData";
+
+			HomeResponseModel model = new HomeResponseModel();
+			model.AlertMessage = "Hello from Model";
+
+			return View(model);
+		}
+
+		
+	}
 }
