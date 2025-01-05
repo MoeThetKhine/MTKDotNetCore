8bc5cf1c93f989567277aae7a868fee89cc9de0a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-05 10:48:54 +0630

    Format Code

--- MTKDotNetCore.MvcApp/Controllers/HomeController.cs
+++ MTKDotNetCore.MvcApp/Controllers/HomeController.cs
@@ -1,13 +1,13 @@
-﻿namespace MTKDotNetCore.MvcApp.Controllers
-{
-    public class HomeController : Controller
-    {
-        private readonly ILogger<HomeController> _logger;
+﻿namespace MTKDotNetCore.MvcApp.Controllers;
 
-        public HomeController(ILogger<HomeController> logger)
-        {
-            _logger = logger;
-        }
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
 
 		public IActionResult Index()
 		{
@@ -19,21 +19,20 @@
 
 			return View(model);
 		}
-        public IActionResult Privacy()
-        {
-            return View();
-        }
-
-        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-        public IActionResult Error()
-        {
-            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-        }
-
-        public IActionResult Index2()
-        {
-            return View();
-        }
-
+    public IActionResult Privacy()
+    {
+        return View();
     }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+
+    public IActionResult Index2()
+    {
+        return View();
+    }
+
 }
