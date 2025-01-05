884446ffd3cc7a790ce171f89bc47666cc92e1bb
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-05 10:41:01 +0630

    Feat : Add Index

--- MTKDotNetCore.MvcApp/Controllers/BlogController.cs
+++ MTKDotNetCore.MvcApp/Controllers/BlogController.cs
@@ -1,12 +1,23 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MTKDotNetCore.MvcApp.Models;
 
 namespace MTKDotNetCore.MvcApp.Controllers
 {
     public class BlogController : Controller
     {
-        public IActionResult Index()
-        {
-            return View();
-        }
-    }
+		private readonly IBlogService _blogService;
+
+		public BlogController(IBlogService blogService)
+		{
+			_blogService = blogService;
+		}
+
+		public IActionResult Index()
+		{
+			var lst = _blogService.GetBlogs();
+			return View(lst);
+		}
+
+		
+	}
 }
