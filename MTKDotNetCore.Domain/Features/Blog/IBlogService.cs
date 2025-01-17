586d97dade7f73efc872ebb7f673fa647f523155
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-17 17:34:27 +0630

    Feat : Update IBlogService

--- MTKDotNetCore.Domain/Features/Blog/IBlogService.cs
+++ MTKDotNetCore.Domain/Features/Blog/IBlogService.cs
@@ -4,7 +4,7 @@ namespace MTKDotNetCore.Domain.Features.Blog
     public interface IBlogService
     {
         TblBlog CreateBlog(TblBlog blog);
-        bool? DelteBlog(int id);
+        bool? DeleteBlog(int id);
         TblBlog GetBlog(int id);
         List<TblBlog> GetBlogs();
         TblBlog PatchBlog(int id, TblBlog blog);
