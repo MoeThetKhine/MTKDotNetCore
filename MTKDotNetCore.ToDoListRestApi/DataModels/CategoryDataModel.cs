baa9bb4e6b876577433c58f44d73724af2f3196a
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 10:53:23 +0630

    -

--- MTKDotNetCore.ToDoListRestApi/DataModels/CategoryDataModel.cs
+++ MTKDotNetCore.ToDoListRestApi/DataModels/CategoryDataModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace MTKDotNetCore.ToDoListRestApi.DataModels;
 
+#region CategoryDataModel
+
 public class CategoryDataModel
 {
     public int CategoryId { get; set; }
     public string CategoryName { get; set; }
     public bool IsDelete { get; set; }
 }
+
+#endregion
