3067a79ac5e9347a77a4f9a6ff2426e3f9c812b3
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 12:00:37 +0630

    -

--- MTKDotNetCore.MiniKpayDapperApi/Models/Deposit/DepositResponseModel.cs
+++ MTKDotNetCore.MiniKpayDapperApi/Models/Deposit/DepositResponseModel.cs
@@ -1,7 +1,11 @@
 ﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.Deposit;
 
+#region DepositResponseModel
+
 public class DepositResponseModel
 {
     public string PhoneNumber { get; set; }
     public decimal Balance { get; set; }
 }
+
+#endregion
