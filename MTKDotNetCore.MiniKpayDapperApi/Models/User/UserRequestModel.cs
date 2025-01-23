74b2b9ac09979c8d8e602669665c272bbcf2cd7d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 11:58:32 +0630

    Format Code

--- MTKDotNetCore.MiniKpayDapperApi/Models/User/UserRequestModel.cs
+++ MTKDotNetCore.MiniKpayDapperApi/Models/User/UserRequestModel.cs
@@ -1,9 +1,8 @@
-﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.User
-{
+﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.User;
+
     public class UserRequestModel
     {
         public string FullName { get; set; }
         public string PhoneNumber { get; set; }
         public string Pin { get; set; }
     }
-}
