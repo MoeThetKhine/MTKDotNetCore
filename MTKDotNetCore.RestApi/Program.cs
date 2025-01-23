d094b2a7936289e9b23cdf247d255d8db1666127
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 10:50:47 +0630

    -

--- MTKDotNetCore.RestApi/Program.cs
+++ MTKDotNetCore.RestApi/Program.cs
@@ -1,5 +1,7 @@
 var builder = WebApplication.CreateBuilder(args);
 
+#region DI
+
 builder.Services.AddDbContext<AppDbContext>(opt =>
 {
     opt.UseSqlServer(builder.Configuration.GetConnectionString("DbConnection"));
@@ -7,6 +9,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 builder.Services.AddScoped<IBlogService, BlogV2Service>();
 builder.Services.AddScoped<IBlogService, BlogService>();
 
+#endregion
 
 builder.Services.AddControllers();
 
