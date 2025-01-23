6183256b29f14f7de73ae5b7a6b2772c639760a8
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 11:47:59 +0630

    -

--- MTKDotNetCore.MiniKpay.Database/Models/Withdraw/WithdrawResponseModel.cs
+++ MTKDotNetCore.MiniKpay.Database/Models/Withdraw/WithdrawResponseModel.cs
@@ -1,7 +1,11 @@
 ﻿namespace MTKDotNetCore.MiniKpay.Database.Models.Withdraw;
 
+#region WithdrawResponseModel
+
 public class WithdrawResponseModel
 {
     public string PhoneNumber { get; set; }
     public decimal Balance { get; set; }
 }
+
+#endregion
