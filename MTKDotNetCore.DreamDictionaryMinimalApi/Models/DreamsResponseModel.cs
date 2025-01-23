8123face79b3a67ec230de8e6a9485921c77322b
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 11:25:09 +0630

    -

--- MTKDotNetCore.DreamDictionaryMinimalApi/Models/DreamsResponseModel.cs
+++ MTKDotNetCore.DreamDictionaryMinimalApi/Models/DreamsResponseModel.cs
@@ -1,7 +1,11 @@
 ﻿namespace MTKDotNetCore.DreamDictionaryMinimalApi.Models;
 
+#region DreamsResponseModel
+
 public class DreamsResponseModel
 {
     public DreamTitle[] BlogHeader { get; set; }
     public DreamAns[] BlogDetail { get; set; }
 }
+
+#endregion
