b2ffdd1b8602166e1e4cf4c777727ad1decd51dc
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 10:43:44 +0630

    Format Code

--- MTKDotNetCore.MvcApp/Models/PaginatedList.cs
+++ MTKDotNetCore.MvcApp/Models/PaginatedList.cs
@@ -1,39 +1,38 @@
-﻿namespace MTKDotNetCore.MvcApp.Models
+﻿namespace MTKDotNetCore.MvcApp.Models;
+
+#region PaginatedList
+
+public class PaginatedList<T>
 {
+	public List<T> Items { get; set; }
+	public int PageIndex { get; set; }
+	public int TotalPages { get; set; }
+
 	#region PaginatedList
 
-	public class PaginatedList<T>
+	public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
 	{
-		public List<T> Items { get; set; }
-		public int PageIndex { get; set; }
-		public int TotalPages { get; set; }
-
-		#region PaginatedList
-
-		public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
-		{
-			PageIndex = pageIndex;
-			TotalPages = (int)Math.Ceiling(count / (double)pageSize);
-			Items = items;
-		}
-
-		#endregion
-
-		public bool HasPreviousPage => PageIndex > 1;
-		public bool HasNextPage => PageIndex < TotalPages;
-
-		#region PaginatedList Create
-
-		public static PaginatedList<T> Create(IQueryable<T> source, int pageIndex, int pageSize)
-		{
-			var count = source.Count();
-			var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
-			return new PaginatedList<T>(items, count, pageIndex, pageSize);
-		}
-
-		#endregion
-
+		PageIndex = pageIndex;
+		TotalPages = (int)Math.Ceiling(count / (double)pageSize);
+		Items = items;
 	}
 
 	#endregion
+
+	public bool HasPreviousPage => PageIndex > 1;
+	public bool HasNextPage => PageIndex < TotalPages;
+
+	#region PaginatedList Create
+
+	public static PaginatedList<T> Create(IQueryable<T> source, int pageIndex, int pageSize)
+	{
+		var count = source.Count();
+		var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
+		return new PaginatedList<T>(items, count, pageIndex, pageSize);
+	}
+
+	#endregion
+
 }
+
+#endregion
