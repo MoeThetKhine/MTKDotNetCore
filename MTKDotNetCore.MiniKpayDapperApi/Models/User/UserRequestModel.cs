79b0d7fcb8f08a2255406706dca5983289595a8f
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 11:59:06 +0630

    -

--- MTKDotNetCore.MiniKpayDapperApi/Models/User/UserRequestModel.cs
+++ MTKDotNetCore.MiniKpayDapperApi/Models/User/UserRequestModel.cs
@@ -1,8 +1,12 @@
 ﻿namespace MTKDotNetCore.MiniKpayDapperApi.Models.User;
 
-    public class UserRequestModel
-    {
-        public string FullName { get; set; }
-        public string PhoneNumber { get; set; }
-        public string Pin { get; set; }
-    }
+#region UserRequestModel
+
+public class UserRequestModel
+{
+    public string FullName { get; set; }
+    public string PhoneNumber { get; set; }
+    public string Pin { get; set; }
+}
+
+#endregion
