09764bfbb4c644f58b7d3c365d85813499a7d8d7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 11:06:40 +0630

    Format Code

--- MTKDotNetCore.ConsoleApp3/PostModel.cs
+++ MTKDotNetCore.ConsoleApp3/PostModel.cs
@@ -1,14 +1,13 @@
 ﻿namespace MTKDotNetCore.ConsoleApp3;
 
+#region PostModel
 
-    #region PostModel
+public class PostModel
+{
+    public int userId { get; set; }
+    public int id { get; set; }
+    public string title { get; set; }
+    public string body { get; set; }
+}
 
-    public class PostModel
-    {
-        public int userId { get; set; }
-        public int id { get; set; }
-        public string title { get; set; }
-        public string body { get; set; }
-    }
-
-    #endregion
+#endregion
