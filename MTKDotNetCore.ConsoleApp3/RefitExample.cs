1c03b8b07ec7e1a79721c54584ea16c0ec0a101c
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 11:05:24 +0630

    Format Code

--- MTKDotNetCore.ConsoleApp3/RefitExample.cs
+++ MTKDotNetCore.ConsoleApp3/RefitExample.cs
@@ -1,35 +1,34 @@
-﻿namespace MTKDotNetCore.ConsoleApp3
+﻿namespace MTKDotNetCore.ConsoleApp3;
+
+public class RefitExample
 {
-    public class RefitExample
+    public async Task Run()
     {
-        public async Task Run()
+        var blogApi = RestService.For<IBlogApi>("https://localhost:7085");
+        var lst = await blogApi.GetBlogs();
+        foreach (var item in lst)
         {
-            var blogApi = RestService.For<IBlogApi>("https://localhost:7085");
-            var lst = await blogApi.GetBlogs();
-            foreach (var item in lst)
-            {
-                Console.WriteLine(item.BlogTitle);
-            }
-
-            var item2 = await blogApi.GetBlog(1);
-            try
-            {
-                var item3 = await blogApi.GetBlog(100);
-            }
-            catch (ApiException ex)
-            {
-                if (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
-                {
-                    Console.WriteLine("No data found.");
-                }
-            }
-
-            var item4 = await blogApi.CreateBlog(new BlogModel
-            {
-                BlogTitle = "test",
-                BlogAuthor = "test",
-                BlogContent = "test",
-            });
+            Console.WriteLine(item.BlogTitle);
         }
+
+        var item2 = await blogApi.GetBlog(1);
+        try
+        {
+            var item3 = await blogApi.GetBlog(100);
+        }
+        catch (ApiException ex)
+        {
+            if (ex.StatusCode == System.Net.HttpStatusCode.NotFound)
+            {
+                Console.WriteLine("No data found.");
+            }
+        }
+
+        var item4 = await blogApi.CreateBlog(new BlogModel
+        {
+            BlogTitle = "test",
+            BlogAuthor = "test",
+            BlogContent = "test",
+        });
     }
 }
