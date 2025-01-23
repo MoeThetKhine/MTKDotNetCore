9ea345bb3fb6bddc804ea065b30804df61fa5a12
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 18:44:51 +0630

    Format Code

--- MTKDotNetCore.ChartWebApp/Models/ApexChartPieChartModel.cs
+++ MTKDotNetCore.ChartWebApp/Models/ApexChartPieChartModel.cs
@@ -1,21 +1,20 @@
-﻿namespace MTKDotNetCore.ChartWebApp.Models
+﻿namespace MTKDotNetCore.ChartWebApp.Models;
+
+public class ApexChartPieChartModel
 {
-	public class ApexChartPieChartModel
-	{
-		public int[] Series { get; set; } 
-		public string[] Labels { get; set; } 
-	}
+	public int[] Series { get; set; } 
+	public string[] Labels { get; set; } 
+}
 
-	public class ApexChartMixedChartModel
-	{
-		public List<SeriesData> Series { get; set; }
-		public string[] Labels { get; set; }
-	}
+public class ApexChartMixedChartModel
+{
+	public List<SeriesData> Series { get; set; }
+	public string[] Labels { get; set; }
+}
 
-	public class SeriesData
-	{
-		public string Name { get; set; }
-		public string Type { get; set; }
-		public int[] Data { get; set; }
-	}
+public class SeriesData
+{
+	public string Name { get; set; }
+	public string Type { get; set; }
+	public int[] Data { get; set; }
 }
