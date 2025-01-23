b34c5d9968ec14edfbb24cce17e98e88f95faff7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 10:43:31 +0630

    -

--- MTKDotNetCore.MvcApp/Models/PaginatedList.cs
+++ MTKDotNetCore.MvcApp/Models/PaginatedList.cs
@@ -1,10 +1,14 @@
 ﻿namespace MTKDotNetCore.MvcApp.Models
 {
+	#region PaginatedList
+
 	public class PaginatedList<T>
 	{
 		public List<T> Items { get; set; }
 		public int PageIndex { get; set; }
 		public int TotalPages { get; set; }
+
+		#region PaginatedList
 
 		public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
 		{
@@ -13,8 +17,12 @@
 			Items = items;
 		}
 
+		#endregion
+
 		public bool HasPreviousPage => PageIndex > 1;
 		public bool HasNextPage => PageIndex < TotalPages;
+
+		#region PaginatedList Create
 
 		public static PaginatedList<T> Create(IQueryable<T> source, int pageIndex, int pageSize)
 		{
@@ -22,5 +30,10 @@
 			var items = source.Skip((pageIndex - 1) * pageSize).Take(pageSize).ToList();
 			return new PaginatedList<T>(items, count, pageIndex, pageSize);
 		}
+
+		#endregion
+
 	}
+
+	#endregion
 }
