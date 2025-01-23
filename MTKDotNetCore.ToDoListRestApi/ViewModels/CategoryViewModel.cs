ae05614e8f944504edcaab71f3a63b818c6612eb
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 10:58:20 +0630

    -

--- MTKDotNetCore.ToDoListRestApi/ViewModels/CategoryViewModel.cs
+++ MTKDotNetCore.ToDoListRestApi/ViewModels/CategoryViewModel.cs
@@ -1,6 +1,10 @@
 ﻿namespace MTKDotNetCore.ToDoListRestApi.ViewModels;
 
+#region Category View Model
+
 public class CategoryViewModel
 {
     public string CategoryName { get; set; }
 }
+
+#endregion
