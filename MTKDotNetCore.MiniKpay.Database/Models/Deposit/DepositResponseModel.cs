5c2e5f8e407261bab71e112b16870ff7dae82b0d
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 11:41:17 +0630

    -

--- MTKDotNetCore.MiniKpay.Database/Models/Deposit/DepositResponseModel.cs
+++ MTKDotNetCore.MiniKpay.Database/Models/Deposit/DepositResponseModel.cs
@@ -1,7 +1,11 @@
 ﻿namespace MTKDotNetCore.MiniKpay.Database.Models.Deposit;
 
+#region Deposit Response Model
+
 public class DepositResponseModel
 {
     public string PhoneNumber { get; set; }
     public decimal Balance { get; set; }
 }
+
+#endregion
