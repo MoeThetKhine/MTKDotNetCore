351fd0085798660d23269f07ebaae809ee601ca7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 15:18:52 +0630

    Feat: Add ApexChartController

---
+++ MTKDotNetCore.ChartWebApp/Controllers/ApexChartController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MTKDotNetCore.ChartWebApp.Controllers
+{
+	public class ApexChartController : Controller
+	{
+		public IActionResult PieChart()
+		{
+			return View();
+		}
+	}
+}
