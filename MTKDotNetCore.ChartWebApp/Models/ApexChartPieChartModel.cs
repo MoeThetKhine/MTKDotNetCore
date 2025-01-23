6f7cef7ba04e303897f25825e72662a3491c9ea7
Moe Thet Khine <soniapoudel03@gmail.com>
2025-01-23 18:33:22 +0630

    Feat : Add ApexChartMixedChartModel in Model

--- MTKDotNetCore.ChartWebApp/Models/ApexChartPieChartModel.cs
+++ MTKDotNetCore.ChartWebApp/Models/ApexChartPieChartModel.cs
@@ -5,4 +5,10 @@
 		public int[] Series { get; set; } 
 		public string[] Labels { get; set; } 
 	}
+
+	public class ApexChartMixedChartModel
+	{
+		public List<SeriesData> Series { get; set; }
+		public string[] Labels { get; set; }
+	}
 }
